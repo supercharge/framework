6a68d9bf90ef3083e4ceba816e9ffdcdbf124a33
Marcus Pöhls <marcus.poehls@gmail.com>
2019-02-07 16:52:31 +0100

    make:auth: copy middleware, models, events

--- foundation/console/commands/make-auth.js
+++ foundation/console/commands/make-auth.js
@@ -28,6 +28,18 @@ class MakeAuth extends BaseCommand {
       'auth/forgot-password.js': 'auth/forgot-password.js',
       'auth/reset-password.js': 'auth/reset-password.js'
     }
+
+    this.middleware = {
+      'auth/session.js': 'auth/session.js'
+    }
+
+    this.models = {
+      'user.js': 'user.js'
+    }
+
+    this.events = {
+      'auth/user-registered.js': 'auth/user-registered.js'
+    }
   }
 
   /**
@@ -51,26 +63,21 @@ class MakeAuth extends BaseCommand {
     await this.run(async () => {
       await this.copyFiles()
 
-      this.success(`\n${this.icon('success')} Authentication scaffolding successful. Generated authentication views and routes.`)
+      this.success(`\n${this.icon('success')} Authentication scaffolding successful.`)
     })
   }
 
   async copyFiles () {
-    await this.ensureDirectories()
     await this.copyViews()
+    await this.useAppLayoutInWelcomeRoute()
+
     // copy CSS styles?
     // copy email template
 
     await this.copyModels()
     await this.copyRoutes()
-
-    // copy event?
-    // copy auth middleware?
-  }
-
-  async ensureDirectories () {
-    await this.ensureDir(Helper.viewsPath('auth'))
-    await this.ensureDir(Helper.routesPath('auth'))
+    await this.copyMiddlware()
+    await this.copyEvents()
   }
 
   async copyViews () {
@@ -92,8 +99,35 @@ class MakeAuth extends BaseCommand {
     console.log()
   }
 
+  async useAppLayoutInWelcomeRoute () {
+    const welcomeRoute = Helper.routesPath('welcome.js')
+    const content = await this.getFileContent(welcomeRoute)
+
+    if (content) {
+      await this.writeFile(
+        welcomeRoute,
+        content.replace(`, null, { layout: 'clean' }`, '')
+      )
+    }
+  }
+
   async copyModels () {
-    // copy user model
+    await forEachSeries(Object.entries(this.models), async ([stub, dest]) => {
+      if (await this.pathExists(Helper.modelsPath(dest))) {
+        if (!await this.confirm(`The model [${dest}] exists already. Replace it?`)) {
+          return
+        }
+      }
+
+      await this.copy(
+        Path.resolve(this.stubsDir, 'models', stub),
+        Helper.modelsPath(dest)
+      )
+
+      this.completed('created', `models/${dest}`)
+    })
+
+    console.log()
 
     // const dependencies = ['mongoose@5.4.3']
     // await this.install(dependencies)
@@ -114,6 +148,8 @@ class MakeAuth extends BaseCommand {
 
       this.completed('created', `routes/${dest}`)
     })
+
+    console.log()
   }
 
   async install (dependencies) {
@@ -122,6 +158,40 @@ class MakeAuth extends BaseCommand {
     this.info(`Installing model dependencies: [${[...dependencies]}]`)
 
     await Execa('npm', ['install', ...dependencies], { cwd: Helper.appRoot() })
+  }
+
+  async copyMiddlware () {
+    await forEachSeries(Object.entries(this.middleware), async ([stub, dest]) => {
+      if (await this.pathExists(Helper.middlewarePath(dest))) {
+        if (!await this.confirm(`The middleware [${dest}] exists already. Replace it?`)) {
+          return
+        }
+      }
+
+      await this.copy(
+        Path.resolve(this.stubsDir, 'middleware', stub),
+        Helper.middlewarePath(dest)
+      )
+
+      this.completed('created', `middleware/${dest}`)
+    })
+  }
+
+  async copyEvents () {
+    await forEachSeries(Object.entries(this.events), async ([stub, dest]) => {
+      if (await this.pathExists(Helper.eventsPath(dest))) {
+        if (!await this.confirm(`The event [${dest}] exists already. Replace it?`)) {
+          return
+        }
+      }
+
+      await this.copy(
+        Path.resolve(this.stubsDir, 'events', stub),
+        Helper.eventsPath(dest)
+      )
+
+      this.completed('created', `events/${dest}`)
+    })
   }
 }
 
