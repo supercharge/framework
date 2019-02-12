f976bb8e3329c93c16db26016a1f3d2038be1773
Marcus Pöhls <marcus.poehls@gmail.com>
2019-02-12 15:24:03 +0100

    helper tests

---
+++ test/unit/helper/helper.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const Path = require('path')
+const Helper = require('../../../helper')
+const BaseTest = require('../../../testing/base-test')
+
+class HelperTest extends BaseTest {
+  beforeEach ({ context }) {
+    Helper.setAppRoot(Path.resolve(__dirname))
+    context.helper = Helper
+  }
+
+  rootPath (t) {
+    t.deepEqual(t.context.helper.appRoot(), Path.resolve(__dirname))
+  }
+
+  resourcePath (t) {
+    t.deepEqual(t.context.helper.resourcePath(), Path.resolve(__dirname, 'resources'))
+  }
+
+  viewsPath (t) {
+    t.deepEqual(t.context.helper.viewsPath(), Path.resolve(__dirname, 'resources/views'))
+  }
+
+  storagePath (t) {
+    t.deepEqual(t.context.helper.storagePath(), Path.resolve(__dirname, 'storage'))
+  }
+
+  modelsPath (t) {
+    t.deepEqual(t.context.helper.modelsPath(), Path.resolve(__dirname, 'app/models'))
+  }
+
+  routesPath (t) {
+    t.deepEqual(t.context.helper.routesPath(), Path.resolve(__dirname, 'app/http/routes'))
+  }
+
+  middlewarePath (t) {
+    t.deepEqual(t.context.helper.middlewarePath(), Path.resolve(__dirname, 'app/http/middleware'))
+  }
+
+  eventsPath (t) {
+    t.deepEqual(t.context.helper.eventsPath(), Path.resolve(__dirname, 'app/events'))
+  }
+
+  listenersPath (t) {
+    t.deepEqual(t.context.helper.listenersPath(), Path.resolve(__dirname, 'app/listeners'))
+  }
+
+  mailsPath (t) {
+    t.deepEqual(t.context.helper.mailsPath(), Path.resolve(__dirname, 'app/mails'))
+  }
+}
+
+module.exports = new HelperTest()
