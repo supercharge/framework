55905d9a0fb85f83552cc7fbb39b72e902683a75
Marcus Poehls <marcus.poehls@gmail.com>
2019-02-12 06:48:29 +0100

    update lifecycle tests

--- test/integration/server/lifecycle.js
+++ test/integration/server/lifecycle.js
@@ -1,44 +1,60 @@
 'use strict'
 
 const Path = require('path')
-const BaseTest = require('@root/testing/base-test')
-const Launch = require(Path.resolve(__appRoot, 'start'))
+const Fs = require('../../../filesystem')
+const Config = require('../../../config')
+const App = require('../../../foundation/application')
+const BaseTest = require('../../../testing/base-test')
 
 class BaseRoutesTest extends BaseTest {
-  before () {
+  constructor () {
+    super()
+    this.appRoot = Path.resolve(__dirname, '..', '..', '..')
+  }
+
+  async before () {
+    Config.set('app.key', 'a'.repeat(32))
+
+    await Fs.ensureDir(Path.resolve(this.appRoot, 'resources/views/helpers'))
+    await Fs.ensureDir(Path.resolve(this.appRoot, 'resources/views/layouts'))
+    await Fs.ensureDir(Path.resolve(this.appRoot, 'resources/views/partials'))
+
     this.stub(console, 'error')
     this.stub(process, 'exit')
   }
 
-  alwaysAfter () {
+  async alwaysAfter () {
     console.error.restore()
     process.exit.restore()
+
+    await Fs.remove(
+      Path.resolve(this.appRoot, 'resources/views')
+    )
   }
 
   async serialStartsAndStopsDefaultServer (t) {
-    this.mockConsole()
+    const app = new App().fromAppRoot(this.appRoot)
+    await app.bootstrapHttpServer()
+    await app.startServer()
 
-    const launch = new Launch()
-    await launch.launchWithFullSpeed()
-
-    const server = launch.getServer()
+    const server = app.getServer()
     t.not(server.info.started, 0)
 
     process.emit('SIGTERM')
     await new Promise(resolve => setTimeout(resolve, 100))
 
     t.is(server.info.started, 0)
-
-    const { stdout } = this.consoleOutput()
-    t.true(stdout.includes('server started at'))
   }
 
   async serialFailsToStartServer (t) {
-    const launch = new Launch()
-    const server = launch.getServer()
+    const app = new App().fromAppRoot(this.appRoot)
+    await app.bootstrapHttpServer()
+    await app.startServer()
+
+    const server = app.getServer()
     const stub = this.stub(server, 'start').throws(new Error())
 
-    await launch.launch()
+    await app.startServer()
 
     this.sinon().assert.called(console.error)
     this.sinon().assert.called(process.exit)
@@ -48,11 +64,13 @@ class BaseRoutesTest extends BaseTest {
   }
 
   async serialRunsShutdownFunctions (t) {
-    const launch = new Launch()
-    const server = launch.getServer()
+    const app = new App().fromAppRoot(this.appRoot)
+    await app.bootstrapHttpServer()
+
+    const server = app.getServer()
     const stub = this.stub(server, 'start').throws(new Error())
 
-    await launch.launch()
+    await app.startServer()
 
     this.sinon().assert.called(console.error)
     this.sinon().assert.called(process.exit)
