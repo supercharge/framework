24edae18b6d44c4924dd2f27978dfaf946e3048f
Marcus Pöhls <marcus.poehls@gmail.com>
2019-02-13 11:17:11 +0100

    add application tests for HTTP part

---
+++ test/integration/foundation/application.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const Path = require('path')
+const Fs = require('../../../filesystem')
+const Config = require('../../../config')
+const Helper = require('../../../helper')
+const BaseTest = require('../../../testing/base-test')
+const Application = require('../../../foundation/application')
+
+class BaseRoutesTest extends BaseTest {
+  constructor () {
+    super()
+    this.appRoot = Path.resolve(__dirname, 'fixtures')
+  }
+
+  async beforeEach () {
+    Config.set('app.key', 'a'.repeat(32))
+  }
+
+  async before () {
+    this.helperStub = this.stub(Helper, 'resourcePath').returns(
+      Path.resolve(this.appRoot, 'resources', 'views')
+    )
+
+    await Fs.ensureDir(Path.resolve(this.appRoot, 'resources/views/helpers'))
+    await Fs.ensureDir(Path.resolve(this.appRoot, 'resources/views/layouts'))
+    await Fs.ensureDir(Path.resolve(this.appRoot, 'resources/views/partials'))
+  }
+
+  async alwaysAfter () {
+    this.helperStub.restore()
+
+    await Fs.remove(
+      Path.resolve(this.appRoot, 'resources')
+    )
+  }
+
+  async serialThrowsWithoutAppRoot (t) {
+    const app = new Application()
+    await t.throwsAsync(app.httpWithFullSpeed())
+  }
+
+  async serialThrowsWithoutAppKey (t) {
+    Config.set('app.key', null)
+    const app = new Application().fromAppRoot(this.appRoot)
+    await t.throwsAsync(app.httpWithFullSpeed())
+  }
+
+  async serialStartsApplication (t) {
+    const app = new Application().fromAppRoot(this.appRoot)
+    await app.httpWithFullSpeed()
+
+    t.truthy(app.getServer().info.started)
+
+    await app.getServer().stop()
+  }
+}
+
+module.exports = new BaseRoutesTest()
