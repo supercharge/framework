413ae1a030c18e24fe902c28ff91118625ac3670
Marcus Poehls <marcus.poehls@gmail.com>
2019-02-19 06:54:39 +0100

    add test for csrf protection plugin

---
+++ test/unit/foundation/http/bootstrap/csrf-protection.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const BaseTest = require('../../../../../testing/base-test')
+const HttpKernel = require('../../../../../foundation/http/kernel')
+const Application = require('../../../../../foundation/application')
+const CsrfProtection = require('../../../../../foundation/http/bootstrap/csrf-protection')
+
+class LoadRoutesTest extends BaseTest {
+  async doesNotRegisterCsrfProtection (t) {
+    const server = new HttpKernel(new Application()).createServer()
+
+    const handler = new CsrfProtection(new Application())
+    await handler.extends(server)
+
+    t.is(server._core.extensions.server.onPreStart.nodes, null)
+  }
+
+  async registersCsrfProtection (t) {
+    const app = new Application()
+    const stub = this.stub(app, 'isRunningTests').returns(false)
+
+    const server = new HttpKernel().createServer()
+
+    const handler = new CsrfProtection(app)
+    await handler.extends(server)
+
+    await server.initialize()
+
+    stub.restore()
+
+    t.is(server._core.extensions.server.onPreStart.nodes.length, 1)
+  }
+}
+
+module.exports = new LoadRoutesTest()
