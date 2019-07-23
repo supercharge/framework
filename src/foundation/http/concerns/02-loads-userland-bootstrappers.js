9bd7e00ebdf35be9e6a7200b13afae68870c3c99
Marcus Pöhls <marcus.poehls@gmail.com>
2019-07-23 15:23:58 +0200

    test bootstrapper loading

--- src/foundation/http/concerns/02-loads-userland-bootstrappers.js
+++ src/foundation/http/concerns/02-loads-userland-bootstrappers.js
@@ -3,27 +3,44 @@
 const Path = require('path')
 const Fs = require('../../../filesystem')
 const Helper = require('../../../helper')
+const Logger = require('../../../logging')
 const RegistersAppPlugins = require('./03-registers-app-plugins')
 
 class LoadUserlandBootstrapper extends RegistersAppPlugins {
   constructor () {
     super()
 
-    this.bootstrapFile = 'bootstrap/app.js'
+    this.bootstrapperFile = 'bootstrap/app.js'
   }
 
   async _loadUserlandBootstrappers () {
-    if (await Fs.exists(this._bootstrapFile())) {
-      const { bootstrappers = [] } = require(this._bootstrapFile())
-
-      return bootstrappers
+    if (await this._hasBootstrapFile()) {
+      return this._loadBootstrappers()
     }
+
+    Logger.debug('Missing bootstrap/app.js file. Skipping bootstrappers while app start.')
 
     return []
   }
 
+  async _hasBootstrapFile () {
+    return Fs.exists(this._bootstrapFile())
+  }
+
   _bootstrapFile () {
-    return Path.resolve(Helper.appRoot(), this.bootstrapFile)
+    return Path.resolve(Helper.appRoot(), this.bootstrapperFile)
+  }
+
+  async _loadBootstrappers () {
+    const { bootstrappers } = require(this._bootstrapFile())
+
+    if (!Array.isArray(bootstrappers)) {
+      Logger.error(`The "bootstrappers" property in bootstrap/app.js must be an array, received ${typeof bootstrappers}. Ignoring the file.`)
+
+      return []
+    }
+
+    return bootstrappers
   }
 }
 
---
+++ test/unit/foundation/http/concerns/fixtures/bootstrappers/not-array.js
@@ -0,0 +1,4 @@
+'use strict'
+
+module.exports = {
+  bootstrappers: require('./test-bootstrapper') }
