33ac631fc4e75581b2fd46d2ff4685a714306d26
Marcus Pöhls <marcus.poehls@gmail.com>
2019-07-29 10:53:56 +0200

    refactor default session auth strategy

--- console/stubs/make/auth/strategies/web.js
+++ console/stubs/make/auth/strategies/web.js
@@ -1,69 +1,25 @@
 'use strict'
 
 const User = require('../../models/user')
-const Config = require('@supercharge/framework/config')
 
-class WebAuthentication {
+class SessionAuth {
   static get name () {
     return 'web'
   }
 
   static get scheme () {
-    'session'
+    return 'session'
   }
 
   async validate (request) {
     const { session } = request
 
     if (!session.userId) {
-      return { valid: false, credentials: null }
+      return { credentials: null }
     }
 
-    const user = await User.findById(session.userId)
-
-    if (!user) {
-      return { valid: false, credentials: null }
-    }
-
-    return { valid: true, credentials: user }
+    return { credentials: await User.findById(session.userId) }
   }
 }
 
-module.exports = WebAuthentication
-
-/**
- * Tba.
- */
-module.exports = {
-  name: 'session',
-  scheme: 'cookie',
-  options: {
-    /**
-     * Tba.
-     */
-    requestDecoratorName: 'session',
-    password: Config.get('app.key'),
-    cookie: Config.get('session.cookie'),
-    isSecure: Config.get('app.isProduction'),
-    ttl: Config.get('session.lifetime'),
-    redirectTo: '/login',
-    appendNext: true,
-
-    /**
-     * Tba.
-     */
-    validateFunc: async (request, session) => {
-      if (!session.userId) {
-        return { valid: false, credentials: null }
-      }
-
-      const user = await User.findById(session.userId)
-
-      if (!user) {
-        return { valid: false, credentials: null }
-      }
-
-      return { valid: true, credentials: user }
-    }
-  }
-}
+module.exports = SessionAuth
