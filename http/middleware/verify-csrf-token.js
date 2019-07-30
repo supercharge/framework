b9e1a5fb00972f18502fa8be265351f8eedff9ac
Marcus Poehls <marcus.poehls@gmail.com>
2019-07-30 06:46:52 +0200

    add comments

--- http/middleware/verify-csrf-token.js
+++ http/middleware/verify-csrf-token.js
@@ -4,16 +4,35 @@ const Boom = require('@hapi/boom')
 const Logger = require('../../logging')
 
 class VerifyCsrfToken {
+  constructor (server) {
+    this.server = server
+  }
+
+  /**
+   * Request lifecycle extension point to verify
+   * the CSRF token.
+   *
+   * @param {Request} request
+   * @param {Toolkit} h
+   */
   async onPostAuth (request, h) {
-    if (await this.canProceedLifecycle(request)) {
-      request.session.regenerateToken()
+    if (await this.canProceed(request)) {
+      this.rotateToken(request)
 
       return h.continue
     }
 
+    console.log('cannot proceed!')
+
     return this.forbidden()
   }
 
+  /**
+   * Append the CSRF token to the response.
+   *
+   * @param {Request} request
+   * @param {Toolkit} h
+   */
   onPreResponse (request, h) {
     this.addCookieToResponse(request, h)
     this.addTokenToViewContext(request, h)
@@ -21,40 +40,131 @@ class VerifyCsrfToken {
     return h.continue
   }
 
-  async canProceedLifecycle (request) {
-    return this.isReading(request) && this.tokensMatch(request)
+  /**
+   * Determines whether the request matches all conditions
+   * to proceed the request lifecycle.
+   *
+   * @param {Request} request
+   *
+   * @returns {Boolean}
+   */
+  async canProceed (request) {
+    return this.isReading(request) || this.isTesting() || this.tokensMatch(request)
   }
 
+  /**
+   * Determines whether the request is requesting data
+   * and not sending input in the request payload.
+   *
+   * @param {Request} request
+   *
+   * @returns {Boolean}
+   */
   isReading (request) {
-    return ['GET', 'HEAD', 'OPTIONS'].includes(request.method)
+    return ['GET', 'HEAD', 'OPTIONS'].includes(request.method.toLocaleUpperCase())
   }
 
+  /**
+   * Determines whether the current environment is testing.
+   *
+   * @returns {Boolean}
+   */
+  isTesting () {
+    return this.server.app.isRunningTests()
+  }
+
+  /**
+   * Compare the CSRF tokens coming with the request
+   * against the stored token in the session.
+   *
+   * @param {Request} request
+   *
+   * @returns {Boolean}
+   */
   async tokensMatch (request) {
     const token = this.getTokenFrom(request)
 
     return token === request.session.token()
   }
 
+  /**
+   * Retrieve the incoming CSRF token from the request.
+   *
+   * @param {Request} request
+   *
+   * @returns {String}
+   */
   getTokenFrom (request) {
     return request.input('_csrfToken') || request.header('x-csrf-token')
   }
 
-  forbidden () {
-    Logger.debug('CSRF token verification failed')
-    throw Boom.forbidden()
+  /**
+   * Regenerate the CSRF token.
+   *
+   * @param {Request} request
+   */
+  rotateToken (request) {
+    request.session.rotateToken()
   }
 
+  /**
+   * Append the `XSRF-Token` to the response
+   * using an encrypted cookie.
+   *
+   * @param {Request} request
+   * @param {Toolkit} h
+   */
   addCookieToResponse (request, h) {
-    return h.cookie('XSRF-Token', request.session.token())
+    return h.cookie('XSRF-TOKEN', request.session.token())
   }
 
+  /**
+   * Append the CSRF token to the response
+   * when rendering a view.
+   *
+   * @param {Request} request
+   */
   addTokenToViewContext (request) {
     const response = request.response
 
-    if (response.variety === 'view') {
-      response.source.context = response.source.context || {}
-      response.source.context['_csrfToken'] = request.session.token()
+    if (this.isBoomError(response)) {
+      return
     }
+
+    if (this.isView(response)) {
+      response.source.context = response.source.context || {}
+      response.source.context._csrfToken = request.session.token()
+    }
+  }
+
+  /**
+   * Determine whether it’s and error response.
+   *
+   * @param {Object} response
+   *
+   * @returns {Boolean}
+   */
+  isBoomError (response) {
+    return response.isBoom
+  }
+
+  /**
+   * Determine whether the response wants to render a view.
+   *
+   * @param {Object} response
+   *
+   * @returns {Boolean}
+   */
+  isView (response) {
+    return response.variety === 'view'
+  }
+
+  /**
+   * Logs a debug message and throws an HTTP forbidden error.
+   */
+  forbidden () {
+    Logger.debug('CSRF token verification failed')
+    throw Boom.forbidden()
   }
 }
 
