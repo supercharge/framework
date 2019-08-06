86e945ba21179077d825e7c8eea4dad5b80d8223
Marcus Pöhls <marcus.poehls@gmail.com>
2019-08-06 17:14:58 +0200

    comments

--- queue/pending-dispatch.js
+++ queue/pending-dispatch.js
@@ -3,17 +3,54 @@
 const QueueManager = require('.')
 
 class PendingDispatch {
+  /**
+   * This class represents an intermediate state of
+   * a queue job that can be further modified on
+   * where to dispatch the actual job.
+   *
+   * @param {Dispatchable} dispatchable
+   */
   constructor (dispatchable) {
     this.queue = null
+    this.connection = undefined
     this.dispatchable = dispatchable
   }
 
+  /**
+   * Dispatch the job with the given `data`.
+   *
+   * @param {*} data
+   */
   async dispatch (data) {
-    return QueueManager.dispatch(this.dispatchable, data, this.queue)
+    return QueueManager.dispatch(this.dispatchable, {
+      data,
+      queue: this.queue,
+      connection: this.connection
+    })
   }
 
+  /**
+   * Set the queue name for the job.
+   *
+   * @param {String} queue
+   *
+   * @returns {PendingDispatch}
+   */
   onQueue (queue) {
     this.queue = queue
+
+    return this
+  }
+
+  /**
+   * Set the queue connection name for the job.
+   *
+   * @param {String} queue
+   *
+   * @returns {PendingDispatch}
+   */
+  onConnection (connection) {
+    this.connection = connection
 
     return this
   }
