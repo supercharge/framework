27cc3d1c8b96098fd90833d3422bc983afc3f145
Marcus Poehls <marcus.poehls@gmail.com>
2019-08-07 05:59:43 +0200

    comments and minor refactoring

--- queue/faktory-queue.js
+++ queue/faktory-queue.js
@@ -9,41 +9,71 @@ class FaktoryQueue {
     this.client = null
   }
 
+  /**
+   * Create a queue connection.
+   *
+   * @param {Object} config
+   *
+   * @returns {FaktoryQueue}
+   */
   async connect (config) {
     this.client = await Faktory.connect(config)
 
     return this
   }
 
+  /**
+   * Close an existing queue connection.
+   */
   async disconnect () {
     await this.client.close()
   }
 
+  /**
+   * Push a new job with `data` to process
+   * onto the given `queue`.
+   *
+   * @param {String} jobName
+   * @param {*} data
+   * @param {String} queue
+   */
   async push (jobName, data, queue) {
-    const job = this.client.job(jobName, data)
-
-    if (queue) {
-      job.queue = queue
-    }
-
-    return this.client.push(job)
+    return this.client.push({
+      queue,
+      args: data,
+      jobtype: jobName
+    })
   }
 
-  async pop (...queues) {
-    const job = await this.client.fetch(...queues)
+  /**
+   * Retrieve the next job from the queue.
+   *
+   * @param  {String} queue
+   *
+   * @returns {Job}
+   */
+  async pop (queue) {
+    const job = await this.client.fetch(queue)
 
     if (job) {
       return new FaktoryJob(job, this.client)
     }
   }
 
-  async size (...queues) {
+  /**
+   * Returns the size of the queue.
+   *
+   * @param  {String} queue
+   *
+   * @returns {Number}
+   */
+  async size (queue) {
     const info = await this.client.info()
 
     return Collect(Object.keys(info.queues))
-      .filter(queueName => queues.includes(queueName))
+      .filter(queueName => queue === queueName)
       .map(queueName => info.queues[queueName])
-      .reduce((sum, amount) => sum + amount)
+      .reduce((sum, jobCountInQueue) => sum + jobCountInQueue, 0)
   }
 }
 
