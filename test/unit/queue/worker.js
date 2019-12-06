e340e5288ec08656df76bddf4a11a68203178af8
Marcus Poehls <marcus.poehls@gmail.com>
2019-12-06 12:28:20 +0100

    wip queue worker test

--- test/unit/queue/worker.js
+++ test/unit/queue/worker.js
@@ -2,35 +2,87 @@
 
 const Queue = require('../../../queue')
 const BaseTest = require('../../../base-test')
+const Job = require('../../../queue/jobs/job')
 const QueueWorker = require('../../../queue/worker')
+const WorkerOptions = require('../../../queue/worker-options')
 
-class SqsJobTest extends BaseTest {
+class QueueWorkerTest extends BaseTest {
   constructor () {
     super()
 
     Queue.addJob(WorkerTestingJob)
   }
 
-  _getWorker () {
-    return new QueueWorker(
-      this._getWorkerOptions()
+  _getWorker (connectionName = 'default', queues, jobs = [new WorkerTestingJob()]) {
+    const worker = new QueueWorker(
+      this._getWorkerOptions(connectionName, queues)
     )
+
+    // set dependencies
+    worker.manager = new TestingQueueManager(connectionName, new TestingQueue(jobs))
+
+    return worker
   }
 
-  _getWorkerOptions () {
-    // TODO
+  _getWorkerOptions (connectionName = 'default', queues) {
+    return new WorkerOptions({
+      queues,
+      connection: connectionName,
+      maxAttempts: 0 // unlimited
+    })
   }
 
-  async jobIsFired (t) {
+  async jobFires (t) {
     const worker = this._getWorker()
-    await worker.getNextJob()
+    const job = await worker.getNextJob()
+
+    const mock = this.mock(job)
+    mock.expects('fire').once().resolves()
+    mock.expects('fail').never().resolves()
+
+    await worker.handle(job)
+
+    mock.restore()
+    mock.verify()
+
+    t.pass()
   }
 }
 
-class WorkerTestingJob {
+class WorkerTestingJob extends Job {
   async handle () {
+    //
+  }
+
+  attempts () {
+    return 0
+  }
+}
+
+class TestingQueue {
+  constructor (jobs) {
+    this.jobs = [].concat(jobs)
+  }
+
+  async pop () {
+    return this.jobs.pop()
+  }
+}
+
+class TestingQueueManager {
+  constructor (name, connection) {
+    this.connections = {
+      [name]: connection
+    }
+  }
+
+  connection (name) {
+    return this.connections[name]
+  }
+
+  stop () {
     //
   }
 }
 
-module.exports = new SqsJobTest()
+module.exports = new QueueWorkerTest()
