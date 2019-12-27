ad76778e2d091fb9375269410904a81ef24ca79b
Marcus Poehls <marcus.poehls@gmail.com>
2019-12-27 20:22:52 +0100

    test queue bootstrapper

---
+++ test/unit/queue/fixtures/app/jobs/test-job.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Dispatchable = require('../../../../../../queue/dispatchable')
+
+class TestJob extends Dispatchable {
+  async handle () {
+    //
+  }
+}
+
+module.exports = TestJob
