2bc19feed7a112040aebbd07bc78eb3161f8ade9
Marcus Poehls <marcus.poehls@gmail.com>
2020-01-08 03:23:19 +0100

    check attempts

--- test/unit/queue/job.js
+++ test/unit/queue/job.js
@@ -2,10 +2,16 @@
 
 const Job = require('../../../queue/jobs/job')
 const BaseTest = require('../../../base-test')
+const SyncJob = require('../../../queue/jobs/sync-job')
 
 class QueueJobTest extends BaseTest {
   async retrievesAttempts (t) {
     const job = new TestingJob()
+    t.is(job.attempts(), 1)
+  }
+
+  async syncJobAttempts (t) {
+    const job = new SyncJob()
     t.is(job.attempts(), 1)
   }
 }
