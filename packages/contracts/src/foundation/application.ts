7dcce75681dc1e5aa9638130f45f9e1625a23972
Marcus Pöhls <marcus.poehls@gmail.com>
2020-05-19 15:43:18 +0200

    extend Application contract

--- packages/contracts/src/foundation/application.ts
+++ packages/contracts/src/foundation/application.ts
@@ -4,6 +4,22 @@ import { ConfigStore } from '../config'
 
 export interface Application {
   /**
+   * Returns the app version.
+   *
+   * @returns {String}
+   */
+  version(): string
+
+  /**
+   * Returns the root path of the application directory.
+   *
+   * @param {String} path - an optional path appended to the config path
+   *
+   * @returns {String}
+   */
+  basePath(): string
+
+  /**
    * Returns the app config store instance.
    *
    * @returns {ConfigStore}
@@ -11,7 +27,7 @@ export interface Application {
   config (): ConfigStore
 
   /**
-   * Returns the configuration path.
+   * Returns the path to the application's configuration files.
    *
    * @param {String} path - an optional path appended to the config path
    *
@@ -20,9 +36,39 @@ export interface Application {
   configPath (path?: string): string
 
   /**
-   * Returns the app version.
+   * Returns the path to the application's resource files.
+   *
+   * @param {String} path - an optional path appended to the resource path
    *
    * @returns {String}
    */
-  version(): string
+  resourcePath (path?: string): string
+
+  /**
+   * Returns the path to the application's storage directory.
+   *
+   * @param {String} path - an optional path appended to the storage path
+   *
+   * @returns {String}
+   */
+  storagePath (path?: string): string
+
+  /**
+   * Returns the environment file of the application. By default, this is `.env`.
+   *
+   * @param {String} path - an optional path appended to the config path
+   *
+   * @returns {String}
+   */
+  environmentFile(): string
+
+  /**
+   * Returns the path to directory of the environment file.
+   * By default, this is the application's base path.
+   *
+   * @param {String} path - an optional path appended to the config path
+   *
+   * @returns {String}
+   */
+  environmentPath(): string
 }
