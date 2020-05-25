8b35f5df9d6da2fea17a3d4d79b1267cd8a318c2
Marcus Poehls <marcus.poehls@gmail.com>
2020-05-25 07:08:24 +0200

    refine logging contract

--- packages/contracts/src/logging/logger.ts
+++ packages/contracts/src/logging/logger.ts
@@ -2,44 +2,58 @@
 
 export interface Logger {
   /**
-   * Log the given `message` at trace level.
-   *
-   * @param message
-   */
-  trace (message: string): void
-
-  /**
    * Log the given `message` at debug level.
    *
    * @param message
    */
-  debug (message: string): void
+  debug (message: string, ...context: any[]): void
 
   /**
    * Log the given `message` at info level.
    *
    * @param message
    */
-  info (message: string): void
+  info (message: string, ...context: any[]): void
+
+  /**
+   * Log the given `message` at trace level.
+   *
+   * @param message
+   */
+  notice (message: string, ...context: any[]): void
 
   /**
    * Log the given `message` at warn level.
    *
    * @param message
    */
-  warn (message: string): void
+  warning (message: string, ...context: any[]): void
 
   /**
    * Log the given `message` at error level.
    *
    * @param message
    */
-  error (message: string): void
+  error (message: string, ...context: any[]): void
 
   /**
-   * Log the given `message` at fatal level.
+   * Log the given `message` at critical level.
    *
    * @param message
    */
-  fatal (message: string): void
+  critical (message: string, ...context: any[]): void
+
+  /**
+   * Log the given `message` at alert level.
+   *
+   * @param message
+   */
+  alert (message: string, ...context: any[]): void
+
+  /**
+   * Log the given `message` at emergency level.
+   *
+   * @param message
+   */
+  emergency (message: string, ...context: any[]): void
 }
