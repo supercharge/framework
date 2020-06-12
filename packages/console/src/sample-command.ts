9a79d81c7dc87c33cebac5e1e99604a5342f8d04
Marcus Pöhls <marcus.poehls@gmail.com>
2020-06-12 10:22:56 +0200

    sample command

---
+++ packages/console/src/sample-command.ts
@@ -0,0 +1,24 @@
+'use strict'
+
+import { Command } from './command'
+
+export class TestCommand extends Command {
+  /**
+   * Returns the command signature.
+   */
+  static signature (): string {
+    return `
+      make:model
+      { file : this part after the colon is an argument description }
+      { name? : this is an optional argument }
+      { --target? : optional option, parsed as boolean }
+      { --option-1 : optional option, parsed as boolean }
+      { --no-config : negated optional option with alias, parsed as boolean and false by default because of the “no-" prefix }
+      { --source= : required option and a value must be passed down when calling this command }
+    `
+  }
+
+  async handle (): Promise<any> {
+    return true
+  }
+}
