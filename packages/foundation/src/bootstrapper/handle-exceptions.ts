32a060a05039546681d044f30570ff1588ea3dd8
Marcus Pöhls <marcus.poehls@gmail.com>
2020-06-12 15:43:23 +0200

    handle exceptions

--- packages/foundation/src/bootstrapper/handle-exceptions.ts
+++ packages/foundation/src/bootstrapper/handle-exceptions.ts
@@ -1,15 +1,33 @@
 'use strict'
 
-import { Bootstrapper, Application } from '@supercharge/contracts'
+// @ts-ignore
+import Youch from 'youch'
+// @ts-ignore
+import toTerminal from 'youch-terminal'
+import { tap } from '@supercharge/goodies'
+import { Bootstrapper } from '@supercharge/contracts'
 
 export class HandleExceptions implements Bootstrapper {
   /**
-   * Run the `bootstrap` method on all application providers
-   * configured in the `bootstrap/app` directory.
-   *
-   * @param app
+   * Bootstrap the application. This will listen for exceptions
+   * crashing your Node.js process. The listeners will print
+   * a message to the terminal before exiting the process.
    */
-  async bootstrap (app: Application) {
-    // TODO
+  async bootstrap () {
+    process
+      .on('uncaughtException', (error: Error) => this.handle(error))
+      .on('unhandledRejection', (error: Error) => this.handle(error))
+  }
+
+  /**
+   * Pretty-print the given `error` to the terminal.
+   *
+   * @param {Error} error
+   */
+  handle (error?: Error): any {
+    return tap(new Youch(error).toJSON(), output => {
+      console.log(toTerminal(output))
+      process.exit(1)
+    })
   }
 }
