e68c492441552a2e91a1b3722048d58b9a602dd0
Marcus Pöhls <marcus.poehls@gmail.com>
2020-09-08 06:20:50 +0200

    add eslint

---
+++ packages/queue/.eslintrc.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+    es2020: true
+  },
+  extends: [
+    'standard-with-typescript'
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/require-await': 0,
+    '@typescript-eslint/strict-boolean-expressions': 0,
+
+  }
+}
