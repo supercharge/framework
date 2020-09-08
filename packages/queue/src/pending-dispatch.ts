3bccc57fe953bc619c1c4a9359576df246cc9462
Marcus Pöhls <marcus.poehls@gmail.com>
2020-09-08 06:28:34 +0200

    refinements

--- packages/queue/src/pending-dispatch.ts
+++ packages/queue/src/pending-dispatch.ts
@@ -1,59 +1,63 @@
 'use strict'
 
+import { tap } from '@supercharge/goodies'
 import { Dispatchable } from './dispatchable'
-import {} from '@super'
+import { Queue as QueueManager } from './index'
 
 export class PendingDispatch {
   /**
    * The queue name to dispatch the job on.
    */
-  private queue: string = 'null'
+  private queue?: string
 
   /**
    * The queue connection name to dispatch the job on.
    */
-  private connection: string= 'null'
+  private connection?: string
 
   /**
    * The queue manager instance.
    */
-  private readonly queueManager: any
+  private readonly queueManager: typeof QueueManager = QueueManager
 
   /**
    * The queue job to dispatch.
    */
   private readonly dispatchable: typeof Dispatchable
 
+  /**
+   * Create a new pending dispatch instance.
+   *
+   * @param {Dispatchable} dispatchable
+   */
   constructor (dispatchable: typeof Dispatchable) {
     this.dispatchable = dispatchable
   }
 
   /**
    * Dispatch the job with the given `data`.
+   *
+   * @param {*} data
    */
-  async dispatch (data: any): Promise<void> {
-    return this.queueManager.dispatch(this.dispatchable, {
-      data,
-      queue: this.queue,
-      connection: this.connection
-    })
+  async dispatch (data: any): Promise<string | number> {
+    return await this.queueManager.dispatch(this.dispatchable, data, this.connection, this.queue)
   }
 
   /**
    * Set the queue name for the job.
    */
   onQueue (name: string): this {
-    this.queue = name
-
-    return this
+    return tap(this, () => {
+      this.queue = name
+    })
   }
 
   /**
    * Set the queue connection name for the job.
    */
   onConnection (connection: string): this {
-    this.connection = connection
-
-    return this
+    return tap(this, () => {
+      this.connection = connection
+    })
   }
 }
