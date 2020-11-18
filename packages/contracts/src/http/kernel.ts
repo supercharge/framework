74e0a07f432a250f01251a2c808e150da56361d8
Marcus Pöhls <marcus.poehls@gmail.com>
2020-11-18 10:30:57 +0100

    extend http kernel contract

--- packages/contracts/src/http/kernel.ts
+++ packages/contracts/src/http/kernel.ts
@@ -1,10 +1,20 @@
 'use strict'
 
+import { Application } from "../foundation";
+
 export interface HttpKernel {
   /**
+   * Returns the app instance.
+   */
+  app(): Application
+
+  /**
+   * Bootstrap the HTTP server to handle requests.
+   */
+  bootstrap(): Promise<void>
+
+  /**
    * Start an HTTP Server instance handling incoming requests.
-   *
-   * @returns {Promise}
    */
   startServer(): Promise<void>
 }
