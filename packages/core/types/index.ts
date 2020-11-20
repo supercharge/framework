94040f06528a01f8f6e3607d76c2577fb2ba9a94
Marcus Pöhls <marcus.poehls@gmail.com>
2020-11-20 12:20:03 +0100

    types

---
+++ packages/core/types/index.ts
@@ -0,0 +1,4 @@
+
+/// <reference path="../node_modules/@supercharge/env/dist/types/index.d.ts" />
+/// <reference path="../node_modules/@supercharge/config/dist/types/index.d.ts" />
+/// <reference path="./application.ts" />
