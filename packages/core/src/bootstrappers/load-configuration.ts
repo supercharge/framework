7cdca79cd0720c0f7113f7084d340392578ae87f
Marcus Pöhls <marcus.poehls@gmail.com>
2020-11-20 12:18:35 +0100

    fix config loading

--- packages/core/src/bootstrappers/load-configuration.ts
+++ packages/core/src/bootstrappers/load-configuration.ts
@@ -1,5 +1,6 @@
 'use strict'
 
+import { extname } from 'path'
 import RequireAll from 'require-all'
 import { Application, Bootstrapper } from '@supercharge/contracts'
 
@@ -15,6 +16,8 @@ export class LoadConfiguration implements Bootstrapper {
     ).forEach(([key, value]) => {
       app.config().set(key, value)
     })
+
+    this.registerConfigBindings(app)
   }
 
   /**
@@ -28,7 +31,62 @@ export class LoadConfiguration implements Bootstrapper {
     return RequireAll({
       dirname: app.configPath(''),
       recursive: true,
-      filter: /(.*)\.js|.ts$/
+      filter: (file: string) => {
+        /**
+         * This `filter` method allows us to change the property name for
+         * the loaded files. Returning the new name from this method
+         * will be used as the key in the resulting object.
+         *
+         * E.g. an `app.ts` or `app.js` file will be translated into `app`
+         * as the key in the resulting object: `{ app: {…} }`.
+         */
+        return this.isJavascriptOrTypescript(file)
+          ? file.replace(extname(file), '')
+          : false
+      },
+      resolve: (file: any) => {
+        /**
+         * This resolves any ESM default exports to avoid a `default` property
+         * in the result. Example:
+         *   `{ app: { default: {…} }` -> `{ app: { {…} }`
+         */
+        return this.esmResolve(file)
+      }
     })
+  }
+
+  /**
+   * Determine whether the given file name is a JS or TS file.
+   *
+   * @param file
+   *
+   * @returns {Boolean}
+   */
+  isJavascriptOrTypescript (file: string): boolean {
+    return file.endsWith('.d.ts')
+      ? false
+      : ['.js', '.ts'].includes(extname(file))
+  }
+
+  /**
+   * Resolves default exports and returns the file’s content.
+   *
+   * @param file
+   *
+   * @returns {*}
+   */
+  esmResolve (file: any): object {
+    return file?.__esModule && file.default
+      ? file.default
+      : file
+  }
+
+  /**
+   * Register the config store instance to the container.
+   *
+   * @param app
+   */
+  registerConfigBindings (app: Application): void {
+    app.container().singleton('supercharge/config', () => app.config())
   }
 }
