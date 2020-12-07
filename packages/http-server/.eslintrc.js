25babeca401f0f4c4bfcb2d904f43e8df8d1dd28
Marcus Pöhls <marcus.poehls@gmail.com>
2020-12-07 16:54:08 +0100

    '@typescript-eslint/return-await': 0,

--- packages/http-server/.eslintrc.js
+++ packages/http-server/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     project: './tsconfig.json'
   },
   rules: {
+    '@typescript-eslint/return-await': 0,
     '@typescript-eslint/triple-slash-reference': 0,
     '@typescript-eslint/strict-boolean-expressions': 0
   }
