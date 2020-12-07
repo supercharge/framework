e403288e5fcbd6996fdbb6fc9ff5830d040b808a
Marcus Pöhls <marcus.poehls@gmail.com>
2020-12-07 16:07:31 +0100

    use NextHandler

--- packages/contracts/src/http/middleware.ts
+++ packages/contracts/src/http/middleware.ts
@@ -1,10 +1,12 @@
 'use strict'
 
-import { HttpContext } from './context'
+import { HttpContext, NextHandler } from './context'
+
+export type MiddlewareCtor = new () => Middleware
 
 export interface Middleware {
   /**
    * Handle the given request.
    */
-  handle(ctx: HttpContext, next: () => Promise<any>): unknown | Promise<unknown>
+  handle(ctx: HttpContext, next: NextHandler): unknown | Promise<unknown>
 }
