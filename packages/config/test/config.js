b88232a7ca8e2f5b85820ddaf566f309aecc0702
Marcus Pöhls <marcus.poehls@gmail.com>
2020-12-21 14:16:25 +0100

    fix tests

--- packages/config/test/config.js
+++ packages/config/test/config.js
@@ -1,60 +1,58 @@
 'use strict'
 
-const Path = require('path')
 const { Config } = require('../dist')
 
 describe('Config', () => {
-  afterEach(() => {
-    Config.clear()
-  })
-
   it('all', () => {
-    Config.set('app.name', 'Supercharge')
-    expect(typeof Config.all()).toEqual('object')
-    expect(Config.all()).toMatchObject({ app: { name: 'Supercharge' } })
+    const config = new Config()
+    config.set('app.name', 'Supercharge')
+
+    expect(typeof config.all()).toEqual('object')
   })
 
   it('get', () => {
-    Config.set('key', 'value')
-    expect(Config.get('key')).toEqual('value')
-    expect(Config.get('unavailable')).toEqual(undefined)
+    const config = new Config({ key: 'value' })
+
+    expect(config.get('key')).toEqual('value')
+    expect(config.get('unavailable')).toEqual(undefined)
   })
 
   it('get nested', async () => {
-    await new ConfigBootstrapper().bootstrap(new App())
-    expect(Config.get('app.nested.key')).toEqual('nested-value')
+    const config = new Config({ app: { nested: { key: 'nested-value' } } })
+
+    expect(config.get('app.nested.key')).toEqual('nested-value')
   })
 
   it('get defaultValue', () => {
-    expect(Config.get('unavailable', 'fallback')).toEqual('fallback')
+    const config = new Config()
+    expect(config.get('unavailable', 'fallback')).toEqual('fallback')
   })
 
   it('set', () => {
-    Config.set('key', 'value')
-    expect(Config.get('key')).toEqual('value')
+    const config = new Config()
+    config.set('key', 'value')
+    expect(config.get('key')).toEqual('value')
 
-    Config.set('key', undefined)
-    expect(Config.get('key')).toBeUndefined()
+    config.set('key', undefined)
+    expect(config.get('key')).toBeUndefined()
   })
 
   it('has', () => {
-    Config.set('app.port', 1234)
-    Config.set('app.env', 'production')
+    const config = new Config()
 
-    expect(Config.has('app.port')).toBeTrue()
-    expect(Config.has('app.name')).toBeFalse()
-    expect(Config.has('app.environment')).toBeFalse()
+    config.set('app.port', 1234)
+    config.set('app.env', 'production')
+
+    expect(config.has('app.port')).toBeTrue()
+    expect(config.has('app.name')).toBeFalse()
+    expect(config.has('app.environment')).toBeFalse()
   })
 
-  it('initializes with values', () => {
-    const Conf = new Config.constructor({ isProduction: true })
+  it('clear', () => {
+    const config = new Config({ key: 'value' })
+    expect(config.has('key')).toBeTrue()
 
-    expect(Conf.has('isProduction')).toBeFalse()
+    config.clear()
+    expect(config.has('key')).toBeFalse()
   })
 })
-
-class App {
-  configPath () {
-    return Path.resolve(__dirname, 'fixtures/config')
-  }
-}
