d26d4e49b5fd2fc8cf7c090b4297119f5d346395
Marcus Pöhls <marcus.poehls@gmail.com>
2020-12-22 12:44:10 +0100

    refine file logger tests

--- packages/logging/test/file-logger.js
+++ packages/logging/test/file-logger.js
@@ -1,18 +1,24 @@
 'use strict'
 
 const Path = require('path')
-const { Logger } = require('../dist')
 const Fs = require('@supercharge/filesystem')
+const { FileLogger } = require('../dist/src/file-logger')
 
 const logFile = Path.resolve(__dirname, 'test.log')
+
+function logWrite () {
+  return new Promise(resolve => {
+    // wait for the first log flushing down to the file
+    setTimeout(resolve, 50)
+  })
+}
 
 describe.only('File Logger', () => {
   beforeAll(async () => {
     await Fs.ensureFile(logFile)
 
-    Logger.setApp(new App())
-    Logger.info('starting tests')
-    await new Promise(resolve => setTimeout(resolve, 50)) // wait for the first log flushing down to the file
+    const logger = new FileLogger({ path: logFile })
+    logger.info('starting tests')
   })
 
   afterAll(async () => {
@@ -20,47 +26,82 @@ describe.only('File Logger', () => {
   })
 
   it('logs debug message to file', async () => {
-    Logger.debug('debug message')
+    const logger = new FileLogger({ path: logFile })
+    logger.debug('debug message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('debug message')
   })
 
   it('logs info message to file', async () => {
-    Logger.info('info message')
+    const logger = new FileLogger({ path: logFile })
+    logger.info('info message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('info message')
   })
 
   it('logs notice message to file', async () => {
-    Logger.notice('notice message')
+    const logger = new FileLogger({ path: logFile })
+    logger.notice('notice message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('notice message')
   })
 
   it('logs warning message to file', async () => {
-    Logger.warning('warning message')
+    const logger = new FileLogger({ path: logFile })
+    logger.warning('warning message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('warning message')
   })
 
   it('logs error message to file', async () => {
-    Logger.error('error message')
+    const logger = new FileLogger({ path: logFile })
+    logger.error('error message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('error message')
   })
 
   it('logs critical message to file', async () => {
-    Logger.critical('critical message')
+    const logger = new FileLogger({ path: logFile })
+    logger.critical('critical message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('critical message')
   })
 
   it('logs alert message to file', async () => {
-    Logger.alert('alert message')
+    const logger = new FileLogger({ path: logFile })
+    logger.alert('alert message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('alert message')
   })
 
   it('logs emergency message to file', async () => {
-    Logger.emergency('emergency message')
+    const logger = new FileLogger({ path: logFile })
+    logger.emergency('emergency message')
+
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).toInclude('emergency message')
   })
 
   it('logs message with context data (object)', async () => {
-    Logger.info('custom message', { name: 'Marcus' })
+    const logger = new FileLogger({ path: logFile })
+    logger.info('custom message', { name: 'Marcus' })
+
+    await logWrite()
 
     const content = await Fs.readFile(logFile)
     expect(content).toInclude('"message":"custom message"')
@@ -68,40 +109,17 @@ describe.only('File Logger', () => {
   })
 
   it('honors the log level', async () => {
-    Logger.drivers = new Map()
-    Logger.setApp(new WarnLevelApp())
+    const logger = new FileLogger({ level: 'warning', path: logFile })
+    logger.debug('should not appear')
 
-    Logger.debug('should not appear')
+    await logWrite()
+
     expect(await Fs.readFile(logFile)).not.toInclude('should not appear')
 
-    Logger.warning('this warning should appear')
+    logger.warning('this warning should appear')
 
     const content = await Fs.readFile(logFile)
     expect(content).toInclude('"message":"this warning should appear"')
     expect(content).toInclude('"level":"warning"')
   })
 })
-
-class App {
-  config () {
-    return {
-      get (configItem) {
-        return configItem === 'logging.driver'
-          ? 'file'
-          : { path: logFile }
-      }
-    }
-  }
-}
-
-class WarnLevelApp {
-  config () {
-    return {
-      get (configItem) {
-        return configItem === 'logging.driver'
-          ? 'file'
-          : { level: 'warning', path: logFile }
-      }
-    }
-  }
-}
