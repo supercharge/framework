463f38ba01d1d0fb5d6c8fb9a1aed1136251409c
Marcus Pöhls <marcus.poehls@gmail.com>
2021-04-16 15:35:10 +0200

    refactorings to ES module

--- packages/core/src/bootstrappers/load-configuration.ts
+++ packages/core/src/bootstrappers/load-configuration.ts
@@ -1,8 +1,8 @@
 'use strict'
 
-import { extname } from 'path'
-import RequireAll from 'require-all'
-import { esmResolve } from '@supercharge/goodies'
+import { extname, parse } from 'path'
+import Fs from '@supercharge/filesystem'
+import Collect from '@supercharge/collections'
 import { Application, Bootstrapper } from '@supercharge/contracts'
 
 export class LoadConfiguration implements Bootstrapper {
@@ -12,10 +12,10 @@ export class LoadConfiguration implements Bootstrapper {
    * @param {Application} app
    */
   async bootstrap (app: Application): Promise<void> {
-    Object.entries(
-      this.loadConfigurationFiles(app)
-    ).forEach(([key, value]) => {
-      app.config().set(key, value)
+    ([] as any[]).concat(
+      await this.loadConfigurations(app)
+    ).forEach(({ name, config }) => {
+      app.config().set(name, config)
     })
   }
 
@@ -26,11 +26,35 @@ export class LoadConfiguration implements Bootstrapper {
    *
    * @returns {object}
    */
-  loadConfigurationFiles (app: Application): object {
-    return RequireAll({
-      dirname: app.configPath(''),
-      recursive: true,
-      filter: (file: string) => {
+  async loadConfigurations (app: Application): Promise<any> {
+    return await this.resolve(
+      await this.loadConfigurationFilesFromDisk(app)
+    )
+  }
+
+  /**
+   * Load the application’s configuration files from the local hard disk.
+   *
+   * @param {Application} app
+   *
+   * @returns {String[]}
+   */
+  async loadConfigurationFilesFromDisk (app: Application): Promise<string[]> {
+    return await Fs.allFiles(
+      app.configPath()
+    )
+  }
+
+  /**
+   * Load the configuration files from the disk.
+   *
+   * @param {Application} app
+   *
+   * @returns {object}
+   */
+  async resolve (configurationFiles: string[]): Promise<any> {
+    return Collect(configurationFiles)
+      .filter(configFile => {
         /**
          * This `filter` method allows us to change the property name for
          * the loaded files. Returning the new name from this method
@@ -39,19 +63,20 @@ export class LoadConfiguration implements Bootstrapper {
          * E.g. an `app.ts` or `app.js` file will be translated into `app`
          * as the key in the resulting object: `{ app: {…} }`.
          */
-        return this.isJavascriptOrTypescript(file)
-          ? file.replace(extname(file), '')
-          : false
-      },
-      resolve: (file: any) => {
+        return this.isJavascriptOrTypescript(configFile)
+      })
+      .map(async configFile => {
         /**
          * This resolves any ESM default exports to avoid a `default` property
          * in the result. Example:
          *   `{ app: { default: {…} }` -> `{ app: { {…} }`
          */
-        return esmResolve(file)
-      }
-    })
+        return {
+          name: parse(configFile).name,
+          config: (await import(configFile)).default
+
+        }
+      })
   }
 
   /**
