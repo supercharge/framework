7ec65eac3fa684d61ee45a1fd1c728eaf80c55e4
Marcus Pöhls <mp@seda.digital>
2021-06-16 10:07:22 +0200

    add comment

--- packages/database/src/model.ts
+++ packages/database/src/model.ts
@@ -4,6 +4,10 @@ import { QueryBuilder } from './query-builder'
 import { MaybeCompositeId, Model as BaseModel, TransactionOrKnex } from 'objection'
 
 export class Model extends BaseModel {
+  /**
+   * Both of these type definitions are needed to make sure that every model
+   * inheriting from this base class has the fluent query builder available.
+   */
   QueryBuilderType!: QueryBuilder<this>
   static QueryBuilder = QueryBuilder
 
