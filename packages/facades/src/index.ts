e1e10694f347de32c3cbb54848f156965a117224
Marcus Pöhls <marcus.poehls@gmail.com>
2021-06-17 06:17:39 +0200

    database facade

--- packages/facades/src/index.ts
+++ packages/facades/src/index.ts
@@ -7,7 +7,7 @@ import { LogFacade } from './logger'
 import { RouteFacade } from './route'
 import { ConfigFacade } from './config'
 import { DatabaseFacade } from './database'
-import { Application, ConfigStore, Database, EnvStore, Logger, HttpRouter, ViewEngine } from '@supercharge/contracts'
+import { Application, ConfigStore, Database as DatabaseContract, EnvStore, Logger, HttpRouter, ViewEngine } from '@supercharge/contracts'
 
 const Log: Logger = new LogFacade() as unknown as Logger
 const Env: EnvStore = new EnvFacade() as unknown as EnvStore
@@ -15,7 +15,7 @@ const App: Application = new AppFacade() as unknown as Application
 const View: ViewEngine = new ViewFacade() as unknown as ViewEngine
 const Route: HttpRouter = new RouteFacade() as unknown as HttpRouter
 const Config: ConfigStore = new ConfigFacade() as unknown as ConfigStore
-const Database: ConfigStore = new DatabaseFacade() as unknown as Database
+const Database: DatabaseContract = new DatabaseFacade() as unknown as DatabaseContract
 
 export * from './facade'
 export { App, Env, Config, Database, Log as Logger, Route, View }
