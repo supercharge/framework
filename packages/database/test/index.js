36a3fc2c0a50b27099b99277632c5593c0956c2f
Marcus Pöhls <mp@seda.digital>
2021-06-17 09:47:31 +0200

    use tap for testing

--- packages/database/test/index.js
+++ packages/database/test/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
-describe('Foundation', () => {
-  it.skip('TODO', () => {
-    expect(true).toBeFalse()
+const { test, skip } = require('tap')
+
+test('Database', async () => {
+  skip('creates connection', async t => {
+    t.fail()
+  })
+
+  skip('fails to create connection for non-available client', async t => {
+    t.fail()
   })
 })
