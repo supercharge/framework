4863bbb727d37abc31b3445fe1a66152a5566bf3
Marcus Pöhls <marcus.poehls@gmail.com>
2021-06-26 22:09:14 +0200

    add prepend

---
+++ packages/view/src/engines/handlebars/helpers/prepend.ts
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Push content to the beginning of an assets stack identified by the
+ * given `stackName`. Check out the `stack` helper for more details.
+ *
+ * @param {String} stackName
+ * @param {Object} context
+ *
+ * @returns {String} stack content to prepend
+ */
+export default function prepend (stackName: string, context: any): void {
+  if (!context) {
+    throw new Error('Provide a name when using the "prepend" handlebars helper.')
+  }
+
+  const stacks = context.data.root.stacks || {}
+  const stack = stacks[stackName] || []
+
+  // @ts-expect-error
+  stack.unshift({ mode: 'prepend', data: context.fn(this) })
+
+  stacks[stackName] = stack
+  context.data.root.stacks = stacks
+}
