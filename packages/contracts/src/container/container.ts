7d64e2db7e42ae22e7dca83cab8446ebe6342ad7
Marcus Pöhls <marcus.poehls@gmail.com>
2021-08-22 16:35:19 +0200

    refine container contract

--- packages/contracts/src/container/container.ts
+++ packages/contracts/src/container/container.ts
@@ -1,7 +1,6 @@
 'use strict'
 
 import { Class } from '../support'
-import { Application } from '../foundation'
 
 export interface Container {
   /**
@@ -10,7 +9,7 @@ export interface Container {
    * @param {String} namespace
    * @param {Function} factory
    */
-  bind (namespace: string, factory: () => any): this
+  bind (namespace: string | Class, factory: (container: Container) => any): this
 
   /**
    * Register a shared binding (singleton) in the container.
@@ -18,21 +17,21 @@ export interface Container {
    * @param {String} namespace
    * @param {Function} factory
    */
-  singleton (namespace: string, factory: (container: Container) => any): this
-
-  /**
-   * Determine whether the given namespace is bound in the container.
-   *
-   * @param {String} namespace
-   */
-  hasBinding (namespace: string): boolean
+  singleton (namespace: string | Class, factory: (container: Container) => any): this
 
   /**
    * Resolve the given namespace from the container.
    *
    * @param {String} namespace
    */
-  make<T> (namespace: string | Class<T, [Application]>): T
+  make<T> (namespace: string | Class): T
+
+  /**
+   * Determine whether the given `namespace` is bound in the container.
+   *
+   * @param {String} namespace
+   */
+  hasBinding (namespace: string | Class): boolean
 
   /**
    * Flush all bindings and resolved instances from the containter.
