54e275b36de09c6d51def3690a9dcae3a09a8e56
Marcus Pöhls <mp@seda.digital>
2021-08-27 10:12:03 +0200

    export HTTP methods type

---
+++ packages/contracts/src/http/methods.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+/**
+ * Defines the list of HTTP methods.
+ */
+export type HttpMethods =
+  'GET' | 'get' |
+  'HEAD' | 'head' |
+  'POST' | 'post' |
+  'PUT' | 'put' |
+  'DELETE' | 'delete' |
+  'PATCH' | 'patch' |
+  'OPTIONS' | 'options'
