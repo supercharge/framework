96d48e061a21ebd58ba24fc35c9ba05dbba4c91e
Marcus Pöhls <marcus.poehls@gmail.com>
2021-08-29 14:01:37 +0200

    add bodyparser tests

--- packages/http/test/middleware/bodyparser.js
+++ packages/http/test/middleware/bodyparser.js
@@ -12,7 +12,7 @@ const appMock = {
   make () {},
   config () {
     return {
-      get () {
+      get (key) {
         return BodyparserConfig
       }
     }
@@ -48,6 +48,71 @@ test('parse json body', async () => {
   })
     .expect(200, { hello: 'Supercharge' })
     .expectHeader('content-type', 'application/json; charset=utf-8')
+})
+
+test('proceeds request without a body', async () => {
+  await fetch('/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .expect(200, {})
+})
+
+test('parse urlencoded body', async () => {
+  await fetch('/', {
+    body: 'hello=Supercharge',
+    method: 'POST',
+    headers: {
+      Accept: 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+    .expect(200, { hello: 'Supercharge' })
+})
+
+test('parse text body', async () => {
+  await fetch('/', {
+    body: 'hello Supercharge',
+    method: 'POST',
+    headers: {
+      Accept: 'text/plain',
+      'Content-Type': 'text/plain'
+    }
+  })
+    .expect(200, 'hello Supercharge')
+})
+
+test('throws for unsupported content type', async () => {
+  await fetch('/', {
+    body: 'Hello Supercharge',
+    method: 'POST',
+    headers: {
+      Accept: 'supercharge/content-type',
+      'Content-Type': 'supercharge/content-type'
+    }
+  })
+    .expect(415, 'Unsupported Media Type')
+})
+
+test('throws when requesting without content-type headers', async () => {
+  await fetch('/', {
+    body: JSON.stringify({ hello: 'Supercharge' }),
+    method: 'POST',
+    headers: { Accept: 'application/json' }
+  })
+    .expect(200, JSON.stringify({ hello: 'Supercharge' }))
+    .expectHeader('content-type', 'text/plain; charset=utf-8')
+})
+
+test('skips parsing on GET request', async () => {
+  await fetch('/', { method: 'GET' }).expect(204)
+})
+
+test('skips parsing on DELETE request', async () => {
+  await fetch('/', { method: 'DELETE' }).expect(204)
 })
 
 test.run()
--- packages/http/test/middleware/fixtures/bodyparser-config.js
+++ packages/http/test/middleware/fixtures/bodyparser-config.js
@@ -6,14 +6,30 @@ module.exports = {
 
   json: {
     limit: '1mb',
-    strict: true
+    contentTypes: [
+      'application/json',
+      'application/*+json',
+      'application/csp-report'
+    ]
   },
 
   text: {
-    limit: '56kb'
+    limit: '56kb',
+    contentTypes: ['text/*']
   },
 
-  formUrlEncoded: {
-    limit: '56kb'
+  form: {
+    limit: '56kb',
+    contentTypes: [
+      'application/x-www-form-urlencoded'
+    ]
+  },
+
+  multipart: {
+    limit: '20mb',
+    maxFields: 1000,
+    contentTypes: [
+      'multipart/form-data'
+    ]
   }
 }
