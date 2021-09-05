f32b0bf3df296b2093f8f7f0c92512ed23d04996
Marcus Pöhls <marcus.poehls@gmail.com>
2021-09-05 07:08:33 +0200

    add tests

--- packages/http/test/header-bag.js
+++ packages/http/test/header-bag.js
@@ -1,11 +1,49 @@
 'use strict'
 
-const { HeaderBag } = require('../dist/header-bag')
+const { test } = require('uvu')
+const expect = require('expect')
+const { HeaderBag } = require('../dist')
 
-describe('HeaderBag', () => {
-  it('get', () => {
-    const bag = new HeaderBag({})
-
-    expect(bag.get()).toEqual(undefined)
-  })
+test('all', () => {
+  expect(new HeaderBag().all()).toEqual({})
+  expect(new HeaderBag({}).all()).toEqual({})
+  expect(
+    new HeaderBag({ accept: 'application/json' }).all()
+  ).toEqual({ accept: 'application/json' })
 })
+
+test('all for keys', () => {
+  const bag = new HeaderBag({ a: 1, b: 2, c: 3 })
+
+  expect(bag.all('a')).toEqual({ a: 1 })
+  expect(bag.all('a', 'b')).toEqual({ a: 1, b: 2 })
+  expect(bag.all(['a', 'b'])).toEqual({ a: 1, b: 2 })
+})
+
+test('get', () => {
+  expect(new HeaderBag({}).get()).toEqual(undefined)
+  expect(new HeaderBag().get('test')).toEqual(undefined)
+  expect(new HeaderBag({ a: 'b' }).get('c')).toEqual(undefined)
+  expect(new HeaderBag({ a: 'b' }).get('a')).toEqual('b')
+})
+
+test('set', () => {
+  expect(new HeaderBag().set('a', 1).all()).toEqual({ a: 1 })
+  expect(new HeaderBag({}).set('a', 1).all()).toEqual({ a: 1 })
+
+  expect(new HeaderBag({ a: 'b' }).set('c', 1).all()).toEqual({ a: 'b', c: 1 })
+
+  const bag = new HeaderBag({ a: 'b' })
+  expect(bag.get('a')).toEqual('b')
+  expect(bag.set('a', 1).get('a')).toEqual(1)
+})
+
+test('has', () => {
+  expect(new HeaderBag({}).has()).toBe(false)
+  expect(new HeaderBag().has('test')).toBe(false)
+  expect(new HeaderBag({ a: 'b' }).has('c')).toBe(false)
+
+  expect(new HeaderBag({ a: 'b' }).has('a')).toBe(true)
+})
+
+test.run()
---
+++ packages/http/test/parameter-bag.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const { test } = require('uvu')
+const expect = require('expect')
+const { ParameterBag } = require('../dist')
+
+test('all', () => {
+  expect(new ParameterBag().all()).toEqual({})
+  expect(new ParameterBag({}).all()).toEqual({})
+  expect(
+    new ParameterBag({ accept: 'application/json' }).all()
+  ).toEqual({ accept: 'application/json' })
+})
+
+test('all for keys', () => {
+  const bag = new ParameterBag({ a: 1, b: 2, c: 3 })
+
+  expect(bag.all('a')).toEqual({ a: 1 })
+  expect(bag.all('a', 'b')).toEqual({ a: 1, b: 2 })
+  expect(bag.all(['a', 'b'])).toEqual({ a: 1, b: 2 })
+})
+
+test('get', () => {
+  expect(new ParameterBag({}).get()).toEqual(undefined)
+  expect(new ParameterBag().get('test')).toEqual(undefined)
+  expect(new ParameterBag({ a: 'b' }).get('c')).toEqual(undefined)
+  expect(new ParameterBag({ a: 'b' }).get('a')).toEqual('b')
+})
+
+test('set', () => {
+  expect(new ParameterBag().set('a', 1).all()).toEqual({ a: 1 })
+  expect(new ParameterBag({}).set('a', 1).all()).toEqual({ a: 1 })
+
+  expect(new ParameterBag({ a: 'b' }).set('c', 1).all()).toEqual({ a: 'b', c: 1 })
+
+  const bag = new ParameterBag({ a: 'b' })
+  expect(bag.get('a')).toEqual('b')
+  expect(bag.set('a', 1).get('a')).toEqual(1)
+})
+
+test('has', () => {
+  expect(new ParameterBag({}).has()).toBe(false)
+  expect(new ParameterBag().has('test')).toBe(false)
+  expect(new ParameterBag({ a: 'b' }).has('c')).toBe(false)
+
+  expect(new ParameterBag({ a: 'b' }).has('a')).toBe(true)
+})
+
+test('has', () => {
+  expect(new ParameterBag().remove('a').all()).toEqual({ })
+  expect(new ParameterBag({}).remove('a').all()).toEqual({ })
+  expect(new ParameterBag({ a: 1, b: 2 }).remove('a').all()).toEqual({ b: 2 })
+  expect(new ParameterBag({ a: 1, b: 2 }).remove('c').all()).toEqual({ a: 1, b: 2 })
+})
+
+test.run()
