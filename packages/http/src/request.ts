c672f7a1a725fd7c218abc9031e783af2c96ea6e
Marcus Pöhls <marcus.poehls@gmail.com>
2021-09-05 07:08:22 +0200

    merge files into request.all()

--- packages/http/src/request.ts
+++ packages/http/src/request.ts
@@ -86,14 +86,15 @@ export class Request implements HttpRequest, InteractsWithContentTypes {
   }
 
   /**
-   * Returns the merged request payload with query parameters. The query
-   * parameters take preceedence over the request payload in case
-   * attributes with the same name are defined in both places.
+   * Returns the merged request payload, files and query parameters. The query parameters
+   * take preceedence over the request payload and files. Files take preceedence over the
+   * request payload in case attributes with the same name are defined in both places.
    */
   all (): Record<string, any> {
     return {
       ...this.payload(),
-      ...this.query().all()
+      ...this.query().all(),
+      ...this.files().all()
     }
   }
 
