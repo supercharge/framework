24c5a6423397d487c957a1a1a75a3e458df61208
Marcus Pöhls <mp@seda.digital>
2021-10-12 11:26:40 +0200

    extend kernel contract

--- packages/contracts/src/http/kernel.ts
+++ packages/contracts/src/http/kernel.ts
@@ -1,10 +1,20 @@
 'use strict'
 
-import { HttpServer } from './server'
 import { Application } from '../foundation'
 import { MiddlewareCtor } from './middleware'
+import { HttpServer, HttpServerHandler } from './server'
 
 export interface HttpKernel {
+  /**
+   * Determine whether the HTTP kernel is already boostrapped.
+   */
+  isBootstrapped (): boolean
+
+  /**
+   * Mark this HTTP kernel as bootstrapped.
+   */
+  markAsBootstrapped (): this
+
   /**
    * Returns the app instance.
    */
@@ -19,6 +29,14 @@ export interface HttpKernel {
    * Start an HTTP Server instance handling incoming requests.
    */
   startServer(): Promise<void>
+
+  /**
+   * Returns a request handler callback compatible with Node.js’ native HTTP server. This method
+   * bootstraps the HTTP server instance by registering routes and middleware before returning
+   * it. This request handler callback is useful during testing when sending requests into
+   * the HTTP server instance with a community package, like Supertest from visionmedia.
+   */
+  serverCallback (): Promise<HttpServerHandler>
 
   /**
    * Returns the application’s global middleware stack. Every middleware
