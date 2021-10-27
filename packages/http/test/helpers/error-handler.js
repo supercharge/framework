fdc4833849849ea5523674b061a93062cc1c1ad5
Marcus Pöhls <marcus.poehls@gmail.com>
2021-10-27 21:10:59 +0200

    refine test error handler

--- packages/http/test/helpers/error-handler.js
+++ packages/http/test/helpers/error-handler.js
@@ -2,10 +2,10 @@
 
 module.exports = class ErrorHandler {
   handle (ctx, error) {
-    // console.log({ ctx, error })
-
     if (error.status || error.statusCode) {
-      return ctx.response.status(error.status || error.statusCode)
+      ctx.response.status(error.status || error.statusCode)
     }
+
+    ctx.response.payload(error.message)
   }
 }
