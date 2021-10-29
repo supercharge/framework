449afdef30cf3e101f9d04e3a7afc7ae2510c941
Marcus Pöhls <mp@seda.digital>
2021-10-29 09:28:04 +0200

    use uvu and c8 instead of jest

--- packages/support/jest.config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-  collectCoverage: true,
-  coverageReporters: ['text', 'html'],
-  setupFilesAfterEnv: ['jest-extended'],
-  testMatch: ['**/test/**/*.[jt]s?(x)']
-}
