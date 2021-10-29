6ea53efa5782ece7c27d4aed9e7a1aa25e95a897
Marcus Pöhls <mp@seda.digital>
2021-10-29 09:26:21 +0200

    use uvu and c8 instead of jest

--- packages/container/test/index.js
+++ packages/container/test/index.js
@@ -1,123 +1,125 @@
 'use strict'
 
+const { test } = require('uvu')
+const expect = require('expect')
 const { Container } = require('../dist')
 
-describe('Container', () => {
-  it('throws for invalid namespace when binding an instance', () => {
-    const container = new Container()
+test('throws for invalid namespace when binding an instance', () => {
+  const container = new Container()
 
-    expect(() => container.bind()).toThrow()
-    expect(() => container.bind(null)).toThrow()
-    expect(() => container.bind('', () => {})).toThrow()
+  expect(() => container.bind()).toThrow()
+  expect(() => container.bind(null)).toThrow()
+  expect(() => container.bind('', () => {})).toThrow()
 
-    expect(() => container.bind({}, () => {})).not.toThrow()
-    expect(container.hasBinding({})).toBeTrue()
+  expect(() => container.bind({}, () => {})).not.toThrow()
+  expect(container.hasBinding({})).toBe(true)
 
-    expect(() => container.bind(123, () => {})).not.toThrow()
-    expect(container.hasBinding(123)).toBeTrue()
+  expect(() => container.bind(123, () => {})).not.toThrow()
+  expect(container.hasBinding(123)).toBe(true)
 
-    expect(() => container.bind(true, () => {})).not.toThrow()
-    expect(container.hasBinding(true)).toBeTrue()
-  })
+  expect(() => container.bind(true, () => {})).not.toThrow()
+  expect(container.hasBinding(true)).toBe(true)
+})
 
-  it('throws when missing the factory function when binding an instance', () => {
-    const container = new Container()
+test('throws when missing the factory function when binding an instance', () => {
+  const container = new Container()
 
-    expect(() => container.bind('namespace')).toThrow()
-  })
+  expect(() => container.bind('namespace')).toThrow()
+})
 
-  it('bind (string namespace)', () => {
-    const container = new Container()
+test('bind (string namespace)', () => {
+  const container = new Container()
 
-    container.bind('namespace', () => 1)
-    container.bind('namespace.binding', () => 2)
-    container.bind('namespace/binding', () => 3)
+  container.bind('namespace', () => 1)
+  container.bind('namespace.binding', () => 2)
+  container.bind('namespace/binding', () => 3)
 
-    expect(container.hasBinding('namespace')).toBeTrue()
-    expect(container.hasBinding('namespace.binding')).toBeTrue()
-    expect(container.hasBinding('namespace/binding')).toBeTrue()
+  expect(container.hasBinding('namespace')).toBe(true)
+  expect(container.hasBinding('namespace.binding')).toBe(true)
+  expect(container.hasBinding('namespace/binding')).toBe(true)
 
-    expect(container.isSingleton('namespace')).toBeFalse()
+  expect(container.isSingleton('namespace')).toBe(false)
 
-    container.singleton('singleton', () => 1)
-    container.singleton('singleton.binding', () => 2)
-    container.singleton('namespace.binding', () => 2)
+  container.singleton('singleton', () => 1)
+  container.singleton('singleton.binding', () => 2)
+  container.singleton('namespace.binding', () => 2)
 
-    expect(container.isSingleton('singleton')).toBeTrue()
-    expect(container.isSingleton('singleton.binding')).toBeTrue()
-    expect(container.isSingleton('namespace.binding')).toBeTrue()
-  })
+  expect(container.isSingleton('singleton')).toBe(true)
+  expect(container.isSingleton('singleton.binding')).toBe(true)
+  expect(container.isSingleton('namespace.binding')).toBe(true)
+})
 
-  it('bind (class constructor as namespace)', () => {
-    class User {
-      constructor (data) {
-        this.data = data
-      }
+test('bind (class constructor as namespace)', () => {
+  class User {
+    constructor (data) {
+      this.data = data
     }
+  }
 
-    const container = new Container()
+  const container = new Container()
 
-    container.bind(User, () => {
-      return new User({ name: 'Supercharge' })
-    })
-
-    expect(container.hasBinding(User)).toBeTrue()
-    expect(container.make(User)).toEqual(new User({ name: 'Supercharge' }))
+  container.bind(User, () => {
+    return new User({ name: 'Supercharge' })
   })
 
-  it('make', () => {
-    const container = new Container()
+  expect(container.hasBinding(User)).toBe(true)
+  expect(container.make(User)).toEqual(new User({ name: 'Supercharge' }))
+})
 
-    container.bind('namespace', () => 1)
+test('make', () => {
+  const container = new Container()
 
-    expect(container.make('namespace')).toBe(1)
-    expect(() => container.make('not-bound')).toThrow('Missing container binding')
+  container.bind('namespace', () => 1)
+
+  expect(container.make('namespace')).toBe(1)
+  expect(() => container.make('not-bound')).toThrow('Missing container binding')
+})
+
+test('make singleton', () => {
+  const container = new Container()
+
+  container.singleton('singleton', () => {
+    return new Singleton()
   })
 
-  it('make singleton', () => {
-    const container = new Container()
+  expect(container.make('singleton')).toBe(container.make('singleton'))
+})
 
-    container.singleton('singleton', () => {
-      return new Singleton()
-    })
-
-    expect(container.make('singleton')).toBe(container.make('singleton'))
-  })
-
-  it('make class', () => {
-    class User {
-      constructor (app) {
-        this.app = app
-      }
+test('make class', () => {
+  class User {
+    constructor (app) {
+      this.app = app
     }
+  }
 
-    const container = new Container()
-    const user = container.make(User)
+  const container = new Container()
+  const user = container.make(User)
 
-    expect(user instanceof User).toBeTrue()
-    expect(user.app).toBe(container)
-  })
+  expect(user instanceof User).toBe(true)
+  expect(user.app).toBe(container)
+})
 
-  it('throws for missing namespace when binding a singleton', () => {
-    const container = new Container()
+test('throws for missing namespace when binding a singleton', () => {
+  const container = new Container()
 
-    expect(() => container.singleton()).toThrow()
-  })
+  expect(() => container.singleton()).toThrow()
+})
 
-  it('flush', () => {
-    const container = new Container()
+test('flush', () => {
+  const container = new Container()
 
-    container.bind('binding', () => 1)
-    container.singleton('singleton', () => 1)
+  container.bind('binding', () => 1)
+  container.singleton('singleton', () => 1)
 
-    expect(container.hasBinding('binding')).toBeTrue()
-    expect(container.isSingleton('singleton')).toBeTrue()
+  expect(container.hasBinding('binding')).toBe(true)
+  expect(container.isSingleton('singleton')).toBe(true)
 
-    container.flush()
+  container.flush()
 
-    expect(container.hasBinding('binding')).toBeFalse()
-    expect(container.isSingleton('singleton')).toBeFalse()
-  })
+  expect(container.hasBinding('binding')).toBe(false)
+  expect(container.isSingleton('singleton')).toBe(false)
 })
 
 class Singleton {}
+
+test.run()
