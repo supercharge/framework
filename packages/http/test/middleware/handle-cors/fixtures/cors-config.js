59d69b8ddbcdb3a5c2cf9ff78f6e7c612914bacb
Marcus Pöhls <marcus.poehls@gmail.com>
2021-10-31 15:07:55 +0100

    wip: cors tests

---
+++ packages/http/test/middleware/handle-cors/fixtures/cors-config.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = {
+  maxAge: 0,
+
+  allowMethods: ['GET, HEAD, PUT, POST, DELETE, PATCH']
+}
---
+++ packages/http/test/middleware/handle-cors/handle-cors.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const { test } = require('uvu')
+const deepmerge = require('deepmerge')
+const Supertest = require('supertest')
+const { isConstructor } = require('@supercharge/classes')
+const defaultCorsConfig = require('./fixtures/cors-config')
+const { HandleCorsMiddleware, Server, Router } = require('../../../dist')
+
+function createAppMock (corsConfig = {}) {
+  return {
+    key () {
+      return 1234
+    },
+    make (key) {
+      if (isConstructor(key)) {
+        // eslint-disable-next-line new-cap
+        return new key(this)
+      }
+
+      if (key === 'route') {
+        return new Router()
+      }
+    },
+    singleton () {},
+    config () {
+      return {
+        get () {
+          return deepmerge.all([{}, defaultCorsConfig, corsConfig])
+        }
+      }
+    }
+  }
+}
+
+async function createHttpServer (bodyparserConfig) {
+  const appMock = createAppMock(bodyparserConfig)
+
+  const server = new Server(appMock).use(HandleCorsMiddleware).use(ctx => {
+    return ctx.response.payload('ok')
+  })
+
+  await server.bootstrap()
+
+  return server.callback()
+}
+
+test('expects 204 on preflight request', async () => {
+  await Supertest(await createHttpServer())
+    .options('/')
+    .set('Origin', 'https://superchargejs.com')
+    .set('Access-Control-Request-Method', 'PUT')
+    .expect(204)
+    .expect('Access-Control-Allow-Origin', 'https://superchargejs.com')
+    .expect('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, DELETE, PATCH')
+})
+
+test('always set `vary` response header to origin', async () => {
+  await Supertest(await createHttpServer())
+    .get('/')
+    .expect(200, 'ok')
+    .expect('Vary', 'Origin')
+})
+
+test.run()
