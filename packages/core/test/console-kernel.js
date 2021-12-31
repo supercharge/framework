75f4cabc3b34b14b976caa5436a221da7b886284
Marcus Pöhls <marcus.poehls@gmail.com>
2021-12-31 13:36:23 +0100

    add console kernel tests

---
+++ packages/core/test/console-kernel.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// const Sinon = require('sinon')
+const Path = require('path')
+const Sinon = require('sinon')
+const { test } = require('uvu')
+const expect = require('expect')
+const { Application, ConsoleKernel } = require('../dist')
+
+const app = Application.createWithAppRoot(Path.resolve(__dirname, 'fixtures'))
+app.config().set('app.key', 1234)
+
+test('static .for(app)', async () => {
+  expect(ConsoleKernel.for(app)).toBeInstanceOf(ConsoleKernel)
+  expect(ConsoleKernel.for(app).app()).toBeInstanceOf(Application)
+})
+
+test('.bootstrappers()', async () => {
+  const bootstrappers = ConsoleKernel.for(app).bootstrappers()
+  expect(bootstrappers.length).toBe(5)
+})
+
+test('.bootstrap()', async () => {
+  const kernel = ConsoleKernel.for(app)
+  await kernel.bootstrap()
+
+  expect(
+    kernel.craft().commands().map(command => {
+      return command.getName()
+    }).toArray()
+  ).toEqual(['help'])
+})
+
+test('.loadFrom()', async () => {
+  const kernel = ConsoleKernel.for(app)
+  await kernel.loadFrom(
+    Path.resolve(__dirname, 'fixtures/app/console/commands')
+  )
+
+  expect(
+    kernel.craft().commands().map(command => {
+      return command.getName()
+    }).toArray()
+  ).toEqual(['help', 'test:command'])
+})
+
+test('.run()', async () => {
+  const kernel = ConsoleKernel.for(app)
+  await kernel.loadFrom(
+    Path.resolve(__dirname, 'fixtures/app/console/commands')
+  )
+
+  const consoleLogStub = Sinon.stub(console, 'log').returns()
+  const terminateStub = Sinon.stub(kernel.craft(), 'terminate').returns()
+
+  await kernel.run(['test:command'])
+
+  expect(terminateStub.calledOnce).toBe(true)
+  expect(consoleLogStub.calledOnce).toBe(true)
+
+  terminateStub.restore()
+  consoleLogStub.restore()
+})
+
+test.run()
