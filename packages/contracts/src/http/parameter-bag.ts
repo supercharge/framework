c58398d652f8adce2afe5151616a8cb5e6ebf2a5
Marcus Pöhls <marcus.poehls@gmail.com>
2022-01-11 05:55:59 +0100

    override get method

--- packages/contracts/src/http/parameter-bag.ts
+++ packages/contracts/src/http/parameter-bag.ts
@@ -1,5 +1,12 @@
 'use strict'
 
+import { Dict } from '..'
 import { InputBag } from './input-bag'
 
-export interface ParameterBag<T> extends InputBag<T> {}
+export interface ParameterBag<T> extends InputBag<T> {
+  /**
+   * Returns the parameter value for the given `name`. This method returns
+   * `undefined` if the input bag doesn’t contain the given `name`.
+   */
+  get<Value = string, Header extends keyof Dict<T> = string> (name: Header, defaultValue?: Value): Value | Dict<T>[Header] | undefined
+}
