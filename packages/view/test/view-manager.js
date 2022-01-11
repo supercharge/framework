7a196c55207e62765685ad39f40483167767f0b0
Marcus Pöhls <marcus.poehls@gmail.com>
2022-01-11 05:56:33 +0100

    tests

--- packages/view/test/view-manager.js
+++ packages/view/test/view-manager.js
@@ -2,9 +2,103 @@
 
 const { test } = require('uvu')
 const expect = require('expect')
+const { makeApp } = require('./helpers')
+const { ViewManager } = require('../dist')
 
-test.skip('View Manager', async () => {
-  expect(true).toBe(false)
+test('renders a view', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  expect(
+    await view.render('test-view')
+  ).toEqual('<p>Test View: </p>\n')
+})
+
+test('throws for missing view file', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  await expect(async () => {
+    await view.render('not-existing-test-view')
+  }).rejects.toThrow('View file does not exist.')
+})
+
+test('renders a view with data', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  expect(
+    await view.render('test-view', { name: 'Supercharge ' })
+  ).toEqual('<p>Test View: Supercharge </p>\n')
+})
+
+test('renders a view with data and layout', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  expect(
+    await view.render('test-view', { name: 'Supercharge ' }, { layout: 'test' })
+  ).toEqual('<div class="layout">\n  <p>Test View: Supercharge </p>\n\n</div>\n')
+})
+
+test('throws for invalid views path', async () => {
+  const app = makeApp()
+  const viewsPath = app.resourcePath('not-existing-views-path')
+  app.config().set('view.handlebars.views', viewsPath)
+
+  const view = new ViewManager(app)
+  await view.boot()
+
+  await expect(async () => {
+    return view.render('test-view')
+  }).rejects.toThrow(`Path to view files not existing. Received ${viewsPath}`)
+})
+
+test('throws when rendering a view with not-existing layout', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  await expect(async () => {
+    await view.render('test-view', { name: 'Supercharge ' }, { layout: 'not-existing' })
+  }).rejects.toThrow()
+})
+
+test('throws for misconfigured layouts path', async () => {
+  const app = makeApp()
+
+  const layoutsPath = app.resourcePath('not-existing-layouts-path')
+  app.config().set('view.handlebars.layouts', layoutsPath)
+
+  const view = new ViewManager(app)
+  await view.boot()
+
+  await expect(async () => {
+    await view.render('test-view', { name: 'Supercharge ' }, { layout: 'test' })
+  }).rejects.toThrow(`Path to view layouts not existing. Received ${layoutsPath}`)
+})
+
+test('throws when rendering a view with not-existing layout', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  await expect(async () => {
+    await view.render('test-view', { name: 'Supercharge ' }, { layout: 'not-existing' })
+  }).rejects.toThrow()
+})
+
+test('exists', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  await view.boot()
+
+  expect(await view.exists('test-view')).toBe(false)
+  expect(await view.exists('not-existing-test-view')).toBe(false)
 })
 
 test.run()
