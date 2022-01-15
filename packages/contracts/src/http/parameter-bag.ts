86a5f96378253903096236ecdfef8764c751798b
Marcus Pöhls <marcus.poehls@gmail.com>
2022-01-15 17:36:05 +0100

    naming

--- packages/contracts/src/http/parameter-bag.ts
+++ packages/contracts/src/http/parameter-bag.ts
@@ -8,5 +8,5 @@ export interface ParameterBag<T> extends InputBag<T> {
    * Returns the parameter value for the given `name`. This method returns
    * `undefined` if the input bag doesn’t contain the given `name`.
    */
-  get<Value = string, Header extends keyof Dict<T> = string> (name: Header, defaultValue?: Value): Value | Dict<T>[Header] | undefined
+  get<Value = string, Parameter extends keyof Dict<T> = string> (name: Parameter, defaultValue?: Value): Value | Dict<T>[Parameter] | undefined
 }
