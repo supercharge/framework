b38c3029e1fdfea0e110a8d96b8019c2d12282fd
Marcus Pöhls <marcus.poehls@gmail.com>
2022-02-27 19:05:40 +0100

    add comment

--- packages/mongodb/src/utils.ts
+++ packages/mongodb/src/utils.ts
@@ -1,5 +1,12 @@
 'use strict'
 
+/**
+ * Determine whether the given `value` is an object.
+ *
+ * @param value
+ *
+ * @returns {Boolean}
+ */
 export function isObject (value: any): value is object {
   return value !== null && typeof value === 'object' && !Array.isArray(value)
 }
