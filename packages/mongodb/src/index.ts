ba2b63423126d46c574d53aa1fb16ff995e68a67
Marcus Pöhls <marcus.poehls@gmail.com>
2022-02-27 19:05:30 +0100

    exports

--- packages/mongodb/src/index.ts
+++ packages/mongodb/src/index.ts
@@ -3,3 +3,7 @@
 export * from './contracts'
 export * from './connection'
 export * from './model'
+export * from './mongodb-manager'
+export * from './mongodb-service-provider'
+export * from './query/builder'
+export * from './utils'
