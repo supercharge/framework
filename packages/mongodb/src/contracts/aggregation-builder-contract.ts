3146f8e9fea4ac048480dc4513cec8e1615f2da3
Marcus Pöhls <mp@seda.digital>
2022-03-07 11:10:48 +0100

    rename file

---
+++ packages/mongodb/src/contracts/aggregation-builder-contract.ts
@@ -0,0 +1,67 @@
+'use strict'
+
+import { ModelObject } from '.'
+
+export type AggregateBuilderCallback = (builder: AggregationBuilder) => unknown
+
+export type AggregatePipeline = Array<AggregatePipelineLimit| AggregatePipelineLookup | AggregatePipelineMatch | AggregatePipelineSkip | AggregatePipelineSort>
+
+export interface AggregationBuilder {
+  /**
+   * Limit the number of returned entries to the given `limit`
+   */
+  limit(limit: AggregatePipelineLimit['$limit']): this
+
+  /**
+   * Skip the given `amount` of documents in the query.
+   */
+  skip(amount: AggregatePipelineSkip['$skip']): this
+
+  /**
+   * Sort the result by the given `columns`.
+   */
+  sort(options: AggregatePipelineSort['$sort']): this
+
+  /**
+   * Appends a $lookup operator to this aggregate pipeline.
+   */
+  lookup(options: AggregatePipelineLookup['$lookup']): this
+
+  /**
+   * Appends a $match operator to this aggregate pipeline.
+   */
+  match(options: AggregatePipelineMatch['$match']): this
+}
+
+export interface AggregatePipelineLimit {
+  /** [`$limit` reference](https://docs.mongodb.com/manual/reference/operator/aggregation/limit/) */
+  $limit: number
+}
+
+export interface AggregatePipelineSkip {
+  /** [`$skip` reference](https://docs.mongodb.com/manual/reference/operator/aggregation/skip/) */
+  $skip: number
+}
+
+export type AggregatePipelineSortDirection = -1 | 1 | 'asc' | 'ascending' | 'desc' | 'descending'
+export interface AggregatePipelineSort {
+  /** [`$sort` reference](https://docs.mongodb.com/manual/reference/operator/aggregation/sort/) */
+  $sort: Record<string, 1 | -1 | { $meta: 'textScore' }>
+}
+
+export interface AggregatePipelineLookup {
+  /** [`$lookup` reference](https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/) */
+  $lookup: {
+    from: string
+    as: string
+    localField?: string
+    foreignField?: string
+    let?: Record<string, any>
+    pipeline?: any[]
+  }
+}
+
+export interface AggregatePipelineMatch {
+  /** [`$match` reference](https://docs.mongodb.com/manual/reference/operator/aggregation/match/) */
+  $match: ModelObject
+}
