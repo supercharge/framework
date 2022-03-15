65e8bfd985252901b818cc70397626dfaa6ac4f4
Marcus Pöhls <mp@seda.digital>
2022-03-15 11:47:55 +0100

    add tests

--- packages/mongodb/test/model-joins.js
+++ packages/mongodb/test/model-joins.js
@@ -115,11 +115,34 @@ test.group('Model Joins (Lookup)', (group) => {
     expect(marcus.orders.every(order => order instanceof Order)).toBe(true)
   })
 
-  test('with | fails for missing relation', async () => {
+  test('with | resolves hasMany to empty array when not finding matches', async () => {
     class User extends Model {
       static get relations () {
         return {
-          order: this.hasOne()
+          orders: this.hasMany(Order).localField('_id').foreignField('userId')
+        }
+      }
+    }
+
+    await User.createMany([
+      { _id: 1, name: 'Marcus' },
+      { _id: 2, name: 'Supercharge' }
+    ])
+
+    await Order.createMany([
+      { _id: 1, userId: 1, quantity: 2, price: 10, item: 'shoes' }
+    ])
+
+    const marcus = await User.findById(2).with('orders')
+    expect(marcus instanceof User).toBe(true)
+    expect(marcus.orders).toEqual([])
+  })
+
+  test('with | fails when joining missing relation name', async () => {
+    class User extends Model {
+      static get relations () {
+        return {
+          order: this.hasOne(Order)
         }
       }
     }
@@ -127,5 +150,44 @@ test.group('Model Joins (Lookup)', (group) => {
     expect(() => {
       User.with('notDefined')
     }).toThrow('Cannot find relations "notDefined" on your "User" model')
+  })
+
+  test('relation builder | fails when not providing a related model', async () => {
+    expect(() => {
+      class User extends Model {
+        static get relations () {
+          return {
+            order: this.hasOne()
+          }
+        }
+      }
+
+      User.with('order')
+    }).toThrow('Missing model class argument in one of your "hasOne" relations inside the "User" model')
+  })
+
+  test('relation builder | resolves localField automatically to _id', async () => {
+    class User extends Model {
+      static get relations () {
+        return {
+          order: this.hasOne(Order).foreignField('userId')
+        }
+      }
+    }
+
+    await User.createMany([
+      { _id: 1, name: 'Marcus' },
+      { _id: 2, name: 'Supercharge' }
+    ])
+
+    await Order.createMany([
+      { _id: 1, userId: 1, quantity: 2, price: 10, item: 'shoes' },
+      { _id: 2, userId: 1, quantity: 1, price: 15, item: 'phones' },
+      { _id: 3, userId: 2, quantity: 4, price: 20, item: 'headphones' }
+    ])
+
+    const marcus = await User.findById(1).with('order')
+    expect(marcus instanceof User).toBe(true)
+    expect(marcus.order instanceof Order).toBe(true)
   })
 })
