ed97e3ccf93d4c5aa635b34c36cb7405db15b451
Marcus Pöhls <marcus.poehls@gmail.com>
2022-05-08 15:59:38 +0200

    typings

--- packages/database/src/model.ts
+++ packages/database/src/model.ts
@@ -1,7 +1,7 @@
 'use strict'
 
 import { QueryBuilder } from './query-builder'
-import { MaybeCompositeId, Model as BaseModel, TransactionOrKnex } from 'objection'
+import { Constructor, MaybeCompositeId, Model as BaseModel, TransactionOrKnex } from 'objection'
 
 export class Model extends BaseModel {
   /**
@@ -14,14 +14,16 @@ export class Model extends BaseModel {
   /**
    * Find an item of this model for the given `id`.
    */
-  static findById<M extends Model> (this: typeof Model, id: MaybeCompositeId, trx?: TransactionOrKnex): QueryBuilder<M, M | undefined> {
+  static findById<M extends Model> (this: Constructor<M>, id: MaybeCompositeId, trx?: TransactionOrKnex): QueryBuilder<M, M | undefined> {
+    // @ts-expect-error
     return this.query(trx).findById(id) as any
   }
 
   /**
    * Delete an item of this model for the given `id`.
    */
-  static deleteById<M extends Model> (this: typeof Model, id: MaybeCompositeId, trx?: TransactionOrKnex): QueryBuilder<M, number> {
+  static deleteById<M extends Model> (this: Constructor<M>, id: MaybeCompositeId, trx?: TransactionOrKnex): QueryBuilder<M, number> {
+    // @ts-expect-error
     return this.query(trx).deleteById(id) as any
   }
 }
