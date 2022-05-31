57ab656bc9e9cb2a7dc356ae1140eb72f51a6182
Marcus Pöhls <mp@seda.digital>
2022-05-31 10:43:47 +0200

    add delete method

--- packages/contracts/src/http/cookie-bag.ts
+++ packages/contracts/src/http/cookie-bag.ts
@@ -19,4 +19,9 @@ export interface CookieBag {
    * Determine whether a cookie exists for the given `name`.
    */
   has(name: string): boolean
+
+  /**
+   * Removes a cookie with the given `name`.
+   */
+  delete(name: string): this
 }
