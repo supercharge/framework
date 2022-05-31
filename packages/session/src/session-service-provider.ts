200775a3681c296646f74fa60011b9e72298d1ac
Marcus Pöhls <mp@seda.digital>
2022-05-31 06:05:20 +0200

    refine registrations and bindings

--- packages/session/src/session-service-provider.ts
+++ packages/session/src/session-service-provider.ts
@@ -1,33 +1,46 @@
 'use strict'
 
+import { SessionManager } from './session-manager'
 import { HttpRequest } from '@supercharge/contracts'
 import { ServiceProvider } from '@supercharge/support'
 
-// export interface ContainerBindings {
-//   'route': Router
-//   'router': Router
-// }
+export interface ContainerBindings {
+  'session': SessionManager
+}
 
 export class SessionServiceProvider extends ServiceProvider {
   /**
    * Register application services to the container.
    */
   override register (): void {
+    this.registerSessionManager()
+  }
+
+  /**
+   * Bind the session instance into the container.
+   */
+  private registerSessionManager (): void {
     this.app().singleton('session', () => {
-      return new SessionManager(this.app().config().get('session'))
+      return new SessionManager(this.app())
     })
   }
 
+  /**
+   * Boot application services.
+   */
   override async boot (): Promise<void> {
-    //
     this.registerRequestMacro()
   }
 
+  /**
+   * Register the `request.session()` macro function to the request constructor.
+   */
   private registerRequestMacro (): void {
     const Request = this.app().make<HttpRequest>('request')
+    const session = this.app().make<SessionManager>('session')
 
-    Request.macro('session', function () {
-      return Session.from(this)
+    Request.macro('session', function (this: HttpRequest) {
+      return session.from(this)
     })
   }
 }
