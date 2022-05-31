02f67c7608042842939ea9d247aa15c3484b9c96
Marcus Pöhls <mp@seda.digital>
2022-05-31 17:12:53 +0200

    wip

--- packages/session/src/drivers/cookie.ts
+++ packages/session/src/drivers/cookie.ts
@@ -1,32 +1,57 @@
 'use strict'
 
-import { SessionDriver, SessionConfig } from '@supercharge/contracts'
+import { SessionDriver, SessionConfig, HttpRequest } from '@supercharge/contracts'
 
 export class CookieSessionDriver implements SessionDriver {
+  /**
+   * Stores the request instance.
+   */
+  private readonly request: HttpRequest
+
+  /**
+   * Stores the session config.
+   */
   private readonly config: SessionConfig
 
   /**
-   * Create a new view manager instance.
+   * Create a new cookie session driver instance.
    *
+   * @param {HttpRequest} config
    * @param {SessionConfig} config
    */
-  constructor (config: SessionConfig) {
+  constructor (config: SessionConfig, request: HttpRequest) {
     this.config = config
+    this.request = request
   }
 
+  /**
+   * Read the session data.
+   */
   async read (sessionId: string): Promise<Record<string, any> | undefined> {
-    throw new Error('Method not implemented.')
+    const value = this.request.cookie(sessionId)
+
+    // TODO
+
+    return {}
   }
 
+  /**
+   * Store the session data.
+   */
   async write (sessionId: string, values: Record<string, any>): Promise<this> {
-    throw new Error('Method not implemented.')
+    const value = JSON.stringify(values)
+
+    this.request.cookies().set(sessionId, value)
+
+    return this
   }
 
+  /**
+   * Delete the session data for the given `sessionId`.
+   */
   async destroy (sessionId: string): Promise<this> {
-    throw new Error('Method not implemented.')
-  }
+    this.request.cookies().delete(sessionId)
 
-  async touch (sessionId: string): Promise<this> {
-    throw new Error('Method not implemented.')
+    return this
   }
 }
