49080c88141005419be51a2a839b4bcb36636426
Marcus Pöhls <mp@seda.digital>
2022-06-03 15:45:21 +0200

    implement start session middleware

--- packages/session/src/middleware/start-session.ts
+++ packages/session/src/middleware/start-session.ts
@@ -1,7 +1,7 @@
 'use strict'
 
-import { HttpContext, NextHandler } from '@supercharge/contracts'
 import { SessionManager } from '../session-manager'
+import { Session, HttpContext, HttpResponse, NextHandler } from '@supercharge/contracts'
 
 export class StartSessionMiddleware {
   /**
@@ -22,7 +22,36 @@ export class StartSessionMiddleware {
    * @param {HttpContext} ctx
    * @param {NextHandler} next
    */
-  async handle (_ctx: HttpContext, next: NextHandler): Promise<void> {
+  async handle ({ request, response }: HttpContext, next: NextHandler): Promise<void> {
+    if (this.isMissingSessionDriver()) {
+      return next()
+    }
+
+    const session = request.session()
+    await session.start()
+
     await next()
+
+    await this.addSessionCookieToResponse(session, response)
+
+    await session.commit()
+  }
+
+  /**
+   * Determine whether a valid session driver is configured.
+   */
+  isMissingSessionDriver (): boolean {
+    return this.sessionManager.sessionConfig().driver() === ''
+  }
+
+  /**
+   * Save the session data to storage.
+   *
+   * @param {HttpRequest} request
+   */
+  async addSessionCookieToResponse (session: Session, response: HttpResponse): Promise<void> {
+    response.cookie(session.name(), session.id(), cookie => {
+      cookie.useConfig(this.sessionManager.sessionConfig().cookie().plain())
+    })
   }
 }
