88a0e27d748f13d7871d0d38616a6ffcd56a609e
Marcus Pöhls <mp@seda.digital>
2022-06-09 06:09:01 +0200

    add test using the memory driver

--- packages/session/test/session-manager.js
+++ packages/session/test/session-manager.js
@@ -4,8 +4,8 @@ const { expect } = require('expect')
 const Supertest = require('supertest')
 const { test } = require('@japa/runner')
 const { setupApp } = require('./helpers')
-const { SessionManager } = require('../dist')
 const { Server } = require('@supercharge/http')
+const { SessionManager, StartSessionMiddleware } = require('../dist')
 
 test.group('Session Manager', () => {
   test('creates a session and generates a new ID if none is present', async () => {
@@ -27,5 +27,34 @@ test.group('Session Manager', () => {
 
     expect(sessionId).toBeDefined()
     expect(String(sessionId).length).toBe(40)
+  })
+
+  test('throws when', async () => {
+  })
+
+  test('use the memory driver', async () => {
+    const app = await setupApp({ driver: 'memory' })
+    const server = new Server(app)
+    const sessionManager = app.make('session')
+
+    expect(sessionManager.sessionConfig().driver()).toBe('memory')
+
+    server
+      .use(StartSessionMiddleware)
+      .use(({ request, response }) => {
+        const session = sessionManager.createFrom(request)
+
+        return response.payload(session.set('foo', 'bar').id())
+      })
+
+    const response = await Supertest(server.callback())
+      .get('/')
+      .expect(200)
+
+    const sessionId = response.text
+
+    expect(sessionId).toBeDefined()
+    expect(String(sessionId).length).toBe(40)
+    expect(await sessionManager.driver().read(sessionId)).toMatchObject({ foo: 'bar' })
   })
 })
