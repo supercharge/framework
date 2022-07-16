c241d9befbdcdf12b906827596130bdfff2b4fa1
Marcus Pöhls <marcus.poehls@gmail.com>
2022-07-16 15:01:25 +0200

    test service provider

---
+++ packages/encryption/test/encryption-service-provider.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Path = require('path')
+const { expect } = require('expect')
+const { test } = require('@japa/runner')
+const Str = require('@supercharge/strings')
+const { Application } = require('@supercharge/application')
+const { Encrypter, EncryptionServiceProvider } = require('../dist')
+
+const app = Application.createWithAppRoot(
+  Path.resolve(__dirname, 'fixtures')
+)
+
+app.config().set('app.key', Str.random(32))
+
+test.group('Encryption Service Provider', () => {
+  test('register encrypter and aliases', async () => {
+    app.register(new EncryptionServiceProvider(app))
+
+    expect(app.make('encrypter')).toBeInstanceOf(Encrypter)
+    expect(app.make('encryption')).toBeInstanceOf(Encrypter)
+  })
+})
