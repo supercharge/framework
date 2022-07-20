4dd76702194d27fc76bed7cc9c020517924a9d74
Marcus Pöhls <mp@seda.digital>
2022-07-20 09:01:51 +0200

    add test

--- packages/view/test/view-manager.js
+++ packages/view/test/view-manager.js
@@ -111,4 +111,15 @@ test('exists', async () => {
   expect(await view.exists('not-existing-test-view')).toBe(false)
 })
 
+test('registerPartial', async () => {
+  const app = makeApp()
+  const view = new ViewManager(app)
+  view.registerPartial('test', '<div id="testing-div">this is a test partial</div>')
+  await view.boot()
+
+  expect(
+    await view.render('test-view-with-in-memory-partial')
+  ).toEqual('<p>Test View: <div id="testing-div">this is a test partial</div></p>\n')
+})
+
 test.run()
