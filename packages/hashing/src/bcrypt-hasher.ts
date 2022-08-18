d72b8e0ceb4e37ce9b9219dfe87416d421382dd6
Marcus Pöhls <mp@seda.digital>
2022-08-18 09:57:55 +0200

    implement bcrypt hasher

--- packages/hashing/src/bcrypt-hasher.ts
+++ packages/hashing/src/bcrypt-hasher.ts
@@ -1,12 +1,13 @@
 'use strict'
 
+import Bcrypt from 'bcrypt'
 import { Hasher as HasherContract, HashConfig } from '@supercharge/contracts'
 
 export class BcryptHasher implements HasherContract {
   /**
    * The cost factor.
    */
-  private readonly rounds: number = 10
+  private readonly rounds: number = 12
 
   /**
    * Create a new instance.
@@ -23,9 +24,7 @@ export class BcryptHasher implements HasherContract {
    * @returns {String}
    */
   async make (value: string): Promise<string> {
-    //
-
-    return value
+    return await Bcrypt.hash(value, this.rounds)
   }
 
   /**
@@ -36,8 +35,29 @@ export class BcryptHasher implements HasherContract {
    * @returns {Boolean}
    */
   async check (plain: string, hashedValue: string): Promise<boolean> {
-    // TODO
+    return await Bcrypt.compare(plain, hashedValue)
+  }
 
-    return plain === hashedValue
+  /**
+   * Determine whether the given hash value has been hashed using the configured options.
+   *
+   * @param {String} hashedValue
+   *
+   * @returns {Boolean}
+   */
+  needsRehash (hashedValue: string): boolean {
+    const hash = hashedValue ?? ''
+
+    if (typeof hash !== 'string') {
+      throw new Error('You must provide a string value as an argument to the "needsRehash" method.')
+    }
+
+    if (hash.startsWith('$2a')) {
+      return true
+    }
+
+    const [_version, rounds, ..._rest] = (hashedValue ?? '').split('$')
+
+    return this.rounds !== Number(rounds)
   }
 }
