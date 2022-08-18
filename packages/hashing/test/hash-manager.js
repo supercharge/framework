eabec8b7ad3549d4fdc4db90b2828139463372ed
Marcus Pöhls <mp@seda.digital>
2022-08-18 10:22:39 +0200

    add tests

--- packages/hashing/test/hash-manager.js
+++ packages/hashing/test/hash-manager.js
@@ -2,10 +2,42 @@
 
 const { expect } = require('expect')
 const { test } = require('@japa/runner')
-// const { HashManager } = require('../dist')
+const { setupApp } = require('./helpers')
+const { HashManager } = require('../dist')
 
 test.group('HashManager', () => {
-  test('hash a value', async () => {
-    expect(true).toBe(false)
-  }).skip()
+  test('make hash', async () => {
+    const app = await setupApp()
+    const hash = new HashManager(app)
+    const value = await hash.make('Supercharge')
+
+    expect(typeof value === 'string').toBe(true)
+    expect(value.startsWith('$2b')).toBe(true)
+  })
+
+  test('check hash', async () => {
+    const app = await setupApp()
+    const hash = new HashManager(app)
+    const value = await hash.make('Supercharge')
+
+    expect(await hash.check('Supercharge', value)).toBe(true)
+    expect(await hash.check('Other-Supercharge', value)).toBe(false)
+  })
+
+  test('needsRehash', async () => {
+    const app = await setupApp()
+    const hash = new HashManager(app)
+
+    expect(() => hash.needsRehash()).toThrow()
+    expect(() => hash.needsRehash(null)).toThrow()
+    expect(() => hash.needsRehash(123)).toThrow()
+    expect(() => hash.needsRehash([])).toThrow()
+    expect(() => hash.needsRehash({})).toThrow()
+
+    expect(hash.needsRehash('$2a')).toBe(true)
+    expect(hash.needsRehash('$2b')).toBe(true)
+
+    const hashValue = await hash.make('foo')
+    expect(hash.needsRehash(hashValue)).toBe(false)
+  })
 })
