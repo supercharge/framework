02bf9030547e09e33f21239c4f477a34d1192108
Marcus Pöhls <mp@seda.digital>
2022-10-01 07:01:52 +0200

    add application config

---
+++ packages/contracts/src/application/config.ts
@@ -0,0 +1,33 @@
+'use strict'
+
+export interface ApplicationConfig {
+  /**
+   * The application name.
+   */
+  name: string
+
+  /**
+   * The application key, used to encrypt data.
+   */
+  key: string
+
+  /**
+   * The application description.
+   */
+  description: string
+
+  /**
+   * The application environment.
+   */
+  env: string
+
+  /**
+   * The application version.
+   */
+  version: string
+
+  /**
+   * Determine whether the application runs behind a proxy server.
+   */
+  runsBehindProxy: boolean
+}
