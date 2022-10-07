1a5781f2612afb45e390d2230f495e0fe9285404
Marcus Pöhls <mp@seda.digital>
2022-10-07 13:51:28 +0200

    make log driver use logging config

--- packages/contracts/src/logging/config.ts
+++ packages/contracts/src/logging/config.ts
@@ -9,7 +9,33 @@ export interface LoggingConfig {
   /**
    * The logging channels config.
    */
-  channels: Record<string, LogChannelConfig & Record<string, any>>
+  channels: LoggingChannels
+}
+
+export interface LoggingChannels {
+  /**
+   * The file channel.
+   */
+  file: FileChannelConfig
+
+  /**
+   * The console channel.
+   */
+  console: ConsoleChannelConfig
+
+  /**
+   * Index signature for other channels.
+   */
+  [key: string]: LogChannelConfig & Record<string, any>
+}
+
+export interface ConsoleChannelConfig extends LogChannelConfig {}
+
+export interface FileChannelConfig extends LogChannelConfig {
+  /**
+   * The log file path.
+   */
+  path: string
 }
 
 export interface LogChannelConfig {
--- packages/logging/src/log-manager.ts
+++ packages/logging/src/log-manager.ts
@@ -3,9 +3,27 @@
 import { FileLogger } from './file-logger'
 import { Manager } from '@supercharge/manager'
 import { ConsoleLogger } from './console-logger'
-import { Logger as LoggingContract } from '@supercharge/contracts'
+import { Application, Logger as LoggingContract, LoggingConfig } from '@supercharge/contracts'
 
 export class LogManager extends Manager implements LoggingContract {
+  /**
+   * Stores the logging config.
+   */
+  private readonly options: LoggingConfig
+
+  /**
+   * Create a new logs manager instance.
+   *
+   * @param {Application} app
+   */
+  constructor (app: Application, config: LoggingConfig) {
+    super(app)
+
+    this.options = config
+
+    this.ensureConfig('logging.driver')
+  }
+
   /**
    * Log the given `message` at debug level.
    *
@@ -84,7 +102,7 @@ export class LogManager extends Manager implements LoggingContract {
    * @returns {String}
    */
   protected defaultDriver (): string {
-    return this.config().get('logging.driver', 'console')
+    return this.options.driver ?? 'console'
   }
 
   /**
@@ -105,9 +123,7 @@ export class LogManager extends Manager implements LoggingContract {
    * @returns {FileLogger}
    */
   protected createFileDriver (): LoggingContract {
-    return new FileLogger(
-      this.config().get('logging.channels.file', {})
-    )
+    return new FileLogger(this.options.channels.file ?? {})
   }
 
   /**
@@ -116,8 +132,6 @@ export class LogManager extends Manager implements LoggingContract {
    * @returns {Logger}
    */
   protected createConsoleDriver (): LoggingContract {
-    return new ConsoleLogger(
-      this.config().get('logging.channels.console', {})
-    )
+    return new ConsoleLogger(this.options.channels.console ?? {})
   }
 }
