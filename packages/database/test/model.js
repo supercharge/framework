e3e1701f9b53d65ad21f19a6170f23e3d7d58ea1
Marcus Pöhls <mp@seda.digital>
2022-10-19 14:10:07 +0200

    move test files

---
+++ packages/database/test/model.js
@@ -0,0 +1,69 @@
+'use strict'
+
+/**
+ * @typedef {import('@supercharge/contracts').Database } Database
+ */
+const { test } = require('uvu')
+const { expect } = require('expect')
+const UserModel = require('./helpers/user-model')
+const { DatabaseServiceProvider } = require('../dist')
+const { makeApp, clearDbDirectory } = require('./helpers')
+
+let app
+
+test.before(async () => {
+  app = makeApp()
+
+  app.register(new DatabaseServiceProvider(app))
+
+  const db = app.make('db')
+  const tableName = UserModel.tableName
+
+  if (await db.schema.hasTable(tableName)) {
+    await db.schema.dropTable(tableName)
+  }
+
+  await db.schema.createTable(tableName, table => {
+    table.increments('id')
+    table.string('name')
+  })
+})
+
+test.after(async () => {
+  const db = app.make('db')
+
+  if (await db.schema.hasTable(UserModel.tableName)) {
+    await db.schema.dropTable(UserModel.tableName)
+  }
+
+  await db.destroy()
+
+  await clearDbDirectory()
+})
+
+test('findById', async t => {
+  await UserModel.query().insert({ name: 'Supercharge' })
+
+  const user = await UserModel.findById(1)
+  expect(user).toEqual({ id: 1, name: 'Supercharge' })
+})
+
+test('finds when findByIdOrFail', async t => {
+  const user = await UserModel.query().insert({ name: 'Supercharge' })
+
+  expect(
+    await UserModel.findById(user.id).orFail()
+  ).toEqual(user)
+})
+
+test('deleteById', async t => {
+  const user = await UserModel.query().insert({ name: 'Supercharge' })
+
+  const deleted = await UserModel.deleteById(user.id)
+  expect(deleted).toBe(1)
+
+  const deletedAgain = await UserModel.deleteById(user.id)
+  expect(deletedAgain).toBe(0)
+})
+
+test.run()
---
+++ packages/database/test/query-builder.js
@@ -0,0 +1,89 @@
+'use strict'
+
+/**
+ * @typedef {import('@supercharge/contracts').Database } Database
+ */
+const { test } = require('uvu')
+const { expect } = require('expect')
+const { makeApp, clearDbDirectory } = require('./helpers')
+const UserModel = require('./helpers/user-model')
+const { DatabaseServiceProvider } = require('../dist')
+
+let app
+
+test.before(async () => {
+  app = makeApp()
+
+  app.register(new DatabaseServiceProvider(app))
+
+  const db = app.make('db')
+  const tableName = UserModel.tableName
+
+  if (await db.schema.hasTable(tableName)) {
+    await db.schema.dropTable(tableName)
+  }
+
+  await db.schema.createTable(tableName, table => {
+    table.increments('id')
+    table.string('name')
+  })
+})
+
+test.before.each(async () => {
+  await UserModel.query().delete()
+})
+
+test.after(async () => {
+  const db = app.make('db')
+
+  if (await db.schema.hasTable(UserModel.tableName)) {
+    await db.schema.dropTable(UserModel.tableName)
+  }
+
+  await db.destroy()
+
+  await clearDbDirectory()
+})
+
+test('orFail', async () => {
+  await expect(
+    UserModel.query().findById(1234).orFail()
+  ).rejects.toThrow('Failed to find instance for "UserModel"')
+})
+
+test('orFail with own error', async () => {
+  await expect(
+    UserModel.query().findById(1234).orFail(() => {
+      throw new Error('Cannot find user for ID 1234')
+    })
+  ).rejects.toThrow('Cannot find user for ID 1234')
+})
+
+test('findAll', async () => {
+  const names = ['Marcus', 'Norman', 'Christian']
+
+  const inserted = await Promise.all(
+    names.map(async name => {
+      return await UserModel.query().insert({ name })
+    })
+  )
+
+  const users = await UserModel.query().orFail()
+  expect(users).toEqual(inserted)
+})
+
+test('findAll orFail', async () => {
+  const names = ['Marcus', 'Norman', 'Christian']
+
+  await Promise.all(
+    names.map(async name => {
+      return await UserModel.query().insert({ name })
+    })
+  )
+
+  await expect(
+    UserModel.query().where('id', '>', 1000).orFail()
+  ).rejects.toThrow('Failed to find instance for "UserModel"')
+})
+
+test.run()
