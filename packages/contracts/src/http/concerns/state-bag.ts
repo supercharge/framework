10384f1927fa8669fc678544513dc9246985926f
Marcus Pöhls <mp@seda.digital>
2023-04-11 10:16:17 +0200

    support request state data

--- packages/contracts/src/http/concerns/state-bag.ts
+++ packages/contracts/src/http/concerns/state-bag.ts
@@ -1,24 +1,34 @@
 'use strict'
 
-import { RequestState } from './request-state'
+/**
+ * This interface defines key-value-pairs stored in the shared request state.
+ * Extend this interface in a userland typing file and use TypeScript’s
+ * declaration merging features to provide IntelliSense in the app.
+ *
+ * We can’t use a Record-like interface with an index signature, because the
+ * index signature allows
+ */
+export type RequestStateData = any
 
-export interface StateBag<Entries extends RequestState = {}> {
+export interface StateBag {
   /**
    * Returns the state object.
    */
-  all(...keys: Array<keyof Entries | string>): Record<string, any>
+  all<K extends keyof RequestStateData> (...keys: K[]): Pick<RequestStateData, K>
+  all<R = Record<string, any>> (...keys: string[]): R
 
   /**
    * Returns the saved state for the given `name`.
    */
-  get<R = any>(name: keyof Entries | string): R | undefined
-  get<R = any>(name: keyof Entries | string, defaultValue: R): R
+  get<K extends keyof RequestStateData> (name: K): RequestStateData[K]
+  get<R = any> (name: string, defaultValue?: R): R
 
   /**
    * Add a key-value-pair to the shared state or an object of key-value-pairs.
    */
-  add (values: Record<string, any>): this
-  add (key: keyof Entries | string, value: any): this
+  add<K extends keyof RequestStateData> (key: K, value: RequestStateData[K]): this
+  add (key: string, value: any): this
+  add (values: RequestStateData): this
 
   /**
    * Merge the given `data` object with the existing shared state.
@@ -28,16 +38,20 @@ export interface StateBag<Entries extends RequestState = {}> {
   /**
    * Determine whether a shared state item exists for the given `name`.
    */
-  has(name: keyof Entries | string): name is keyof Entries
+  has<K extends keyof RequestStateData> (name: K): boolean
+  has (name: string): boolean
+
   /**
    * Determine whether the shared state is missing an item for the given `name`.
    */
-  isMissing (name: keyof Entries | string): name is string
+  isMissing<K extends keyof RequestStateData> (name: K): boolean
+  isMissing (name: string): boolean
 
   /**
    * Remove the shared state item for the given `name`.
    */
-  remove(name: keyof Entries | string): this
+  remove<K extends keyof RequestStateData> (name: K): this
+  remove (name: string): this
 
   /**
    * Removes all shared state items.
