a6e5fbd3584ab592f8eed856316083b71d1be227
Marcus Pöhls <mp@seda.digital>
2023-07-23 11:45:30 +0200

    export ScryptValidationError

--- packages/hashing/src/index.ts
+++ packages/hashing/src/index.ts
@@ -3,4 +3,5 @@
 export { BcryptHasher } from './bcrypt-hasher'
 export { ScryptHasher } from './scrypt-hasher'
 export { HashManager } from './hash-manager'
+export { ScryptValidationError } from './scrypt-validation-error'
 export { HashingServiceProvider } from './hashing-service-provider'
