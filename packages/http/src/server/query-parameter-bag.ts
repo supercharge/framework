5ee1166d9c2ca5c3ff046fa9f57dabf0266a8c79
Marcus Pöhls <mp@seda.digital>
2023-10-28 13:55:11 +0200

    add QueryParameterBag

---
+++ packages/http/src/server/query-parameter-bag.ts
@@ -0,0 +1,31 @@
+'use strict'
+
+import { ParameterBag } from './parameter-bag.js'
+import { QueryParameterBag as QueryParameterBagContract } from '@supercharge/contracts'
+
+export class QueryParameterBag<T> extends ParameterBag<T> implements QueryParameterBagContract<T> {
+  /**
+   * Returns the query string created from all items in this query parameter bag,
+   * without the leading question mark `?`.
+   *
+   * **Notice:** the returned querystring is encoded. Node.js automatically
+   * encodes the querystring to ensure a valid URL. Some characters would
+   * break the URL string otherwise. This way ensures the valid string.
+   */
+  toQuerystring (): string {
+    return new URLSearchParams(
+      this.all() as Record<string, string>
+    ).toString()
+  }
+
+  /**
+   * Returns the decoded querystring by running the result of `toQuerystring`
+   * through `decodeURIComponent`. This method is useful to debug during
+   * development. It’s recommended to use `toQuerystring` in production.
+   */
+  toQuerystringDecoded (): string {
+    return decodeURIComponent(
+      this.toQuerystring()
+    )
+  }
+}
