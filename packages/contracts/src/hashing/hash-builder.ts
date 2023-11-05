ff33ac1b17a69c0daf96bd95b693d5c39daeb897
Marcus Pöhls <mp@seda.digital>
2023-11-05 05:02:35 +0100

    add comments

--- packages/contracts/src/hashing/hash-builder.ts
+++ packages/contracts/src/hashing/hash-builder.ts
@@ -9,6 +9,18 @@ export interface HashBuilderConfig {
 }
 
 export interface HashBuilder {
+  /**
+   * Set the input encoding for the related value.
+   */
   inputEncoding(inputEncoding: Encoding): this
+
+  /**
+   * Calculate the final hash string value.
+   */
+  digest (encoding: BinaryToTextEncoding): void
+
+  /**
+   * This is an alias for the `digest` method that calculates the final hash string.
+   */
   toString(outputEncoding: BinaryToTextEncoding): void
 }
