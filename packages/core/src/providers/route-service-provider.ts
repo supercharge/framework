fcb5bc539b7d1c7818489cef8057c0243bbd3158
Marcus Pöhls <mp@seda.digital>
2023-11-14 10:29:26 +0100

    use dynamic import to load route files

--- packages/core/src/providers/route-service-provider.ts
+++ packages/core/src/providers/route-service-provider.ts
@@ -12,48 +12,34 @@ export class RouteServiceProvider extends ServiceProvider {
    * Register application services to the container.
    */
   override register (): void {
-    this.booted(() => {
-      this.runRouteLoadingCallbacks()
+    this.booted(async () => {
+      await this.runRouteLoadingCallbacks()
     })
   }
 
   /**
    * Run the registered route-loading callbacks.
    */
-  protected runRouteLoadingCallbacks (): void {
-    this.loadRoutesCallbacks.forEach(callback => {
-      callback()
-    })
+  protected async runRouteLoadingCallbacks (): Promise<void> {
+    for (const callback of this.loadRoutesCallbacks) {
+      await callback()
+    }
   }
 
   /**
    * Load the routes file from the given `path`.
    */
   loadRoutesFrom (path: string): this {
-    return this.loadRoutesUsing(() => {
-      this.clearRequireCacheFor(path)
-      require(path)
+    return this.loadRoutesUsing(async () => {
+      // we may need to bypass the module cache, like by appending a query parameter `${path}?update=${Date.now()}`
+      await import(path)
     })
-  }
-
-  /**
-   * Delete the require cache entry for the given `path` if it exists.
-   *
-   * @see https://nodejs.org/api/modules.html#modules_caching
-   */
-  protected clearRequireCacheFor (path: string): this {
-    if (require.cache[path]) {
-      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-      delete require.cache[path]
-    }
-
-    return this
   }
 
   /**
    * Register a callback that will be used to load the application’s routes.
    */
-  loadRoutesUsing (callback: () => void): this {
+  loadRoutesUsing (callback: () => Promise<unknown> | unknown): this {
     return tap(this, () => {
       this.loadRoutesCallbacks.push(callback)
     })
