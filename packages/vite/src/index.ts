85b1e04d29637dfce5f50d7a44adc0825dc7830f
Marcus Pöhls <mp@seda.digital>
2023-12-10 06:31:21 +0100

    use InertiaPageNotFoundError

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -7,7 +7,7 @@ export { supercharge }
 export { HotReloadFile } from './plugin/hotfile.js'
 export { PluginConfigContract, DevServerUrl } from './plugin/types.js'
 
-export { resolvePageComponent } from './inertia-helpers.js'
+export { resolvePageComponent, InertiaPageNotFoundError } from './inertia-helpers.js'
 export { Vite, ViteTagAttributes } from './backend/vite.js'
 export { ViteConfig } from './backend/vite-config.js'
 export { ViteServiceProvider } from './vite-service-provider.js'
--- packages/vite/src/inertia-helpers.ts
+++ packages/vite/src/inertia-helpers.ts
@@ -1,4 +1,16 @@
 
+export class InertiaPageNotFoundError extends Error {
+  /**
+   * Create a new instance.
+   */
+  constructor (path: string) {
+    super(`Inertia page not found: ${path}`)
+
+    this.name = 'PageNotFoundError'
+    InertiaPageNotFoundError.captureStackTrace(this, this.constructor)
+  }
+}
+
 /**
  * Resolves the inertia page component for the given `path` from the available `pages`.
  *
@@ -16,7 +28,7 @@ export async function resolvePageComponent<T> (path: string, pages: Record<strin
   const page = pages[path]
 
   if (page == null) {
-    throw new Error(`Inertia page not found: ${path}`)
+    throw new InertiaPageNotFoundError(path)
   }
 
   return typeof page === 'function'
