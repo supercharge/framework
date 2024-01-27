b92cfaa53bb8c7e4ac67b80b21192d1300ac04f0
Marcus Pöhls <mp@seda.digital>
2024-01-27 14:09:31 +0100

    add app() method in CLI command

--- packages/console/src/command.ts
+++ packages/console/src/command.ts
@@ -6,24 +6,33 @@ export class Command extends CedarCommand {
   /**
    * The Supercharge application instance.
    */
-  private _supercharge: Application | undefined
+  private superchargeApp: Application | undefined
+
+  /**
+   * Returns the Supercharge application instance. This method is an alias for
+   * the `supercharge()` method. This method aligns the console app with the
+   * rest of the framework, because there’s the `.app()` method elsewhere.
+   */
+  app (): Application {
+    return this.supercharge()
+  }
 
   /**
    * Returns the Supercharge application instance.
    */
   supercharge (): Application {
-    if (!this._supercharge) {
-      throw new Error()
+    if (!this.superchargeApp) {
+      throw new Error('Missing Supercharge application instance. You must set it via the `setSupercharge` method when registering this command')
     }
 
-    return this._supercharge
+    return this.superchargeApp
   }
 
   /**
    * Set the supercharge application instance for this command.
    */
-  setSupercharge (app: Application): Command {
-    this._supercharge = app
+  setSupercharge (app: Application): this {
+    this.superchargeApp = app
 
     return this
   }
